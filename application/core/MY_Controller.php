afd08688ec2eba16da88fd322659a6ff792a68b6
German Polovnikov <polovnikov.g@gmail.com>
2019-12-23 23:26:33 +0200

    Base - controller, model

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,7 +1,90 @@
 <?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
 /**
- * Created by PhpStorm.
- * User: german
- * Date: 23.12.19
- * Time: 23:24
+ * Class Base_Controller
+ *
+ * @property Template template
+ * @property User_model user_model
+ * @property Useractivity_model useractivity_model
+ * @property Dashboard_model dashboard_model
+ * @property Menuitems_model menuitems_model
+ * @property Suppliers_model suppliers_model
+ * @property Items_model items_model
+ * @property Sections_model sections_model
+ * @property Categories_model categories_model
  */
+
+
+class Base_Controller extends CI_Controller
+{
+}
+
+class MY_Controller extends Base_Controller
+{
+
+    public $success_result =1;
+    public $error_result = 0;
+    public $USR_ROLE='';
+    public $USR_ID='';
+    public $USER_NAME='';
+    public $URER_LOGO='';
+    public $USER_EMAIL='';
+
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->library('user_agent');
+        $this->load->model('user_model');
+        $this->load->model('useractivity_model');
+        $this->load->model('menuitems_model');
+        date_default_timezone_set('America/New_York');
+
+        $user = $this->user_model->current_user();
+        if ($user['result']==0) {
+            if ($this->func->isAjax()) {
+                $this->func->ajaxResponse(array('url'=>'/login'),'Your connection has been lost. Please log in');
+            } else {
+                redirect('/login');
+            }
+        }
+
+        $userdat = $user['data'];
+        $this->USR_ROLE = $userdat['user_logged_in'];
+        $this->USR_ID = $userdat['id'];
+        $this->USER_NAME = $userdat['user_name'];
+        $this->URER_LOGO = $userdat['user_logo'];
+        $this->USER_EMAIL = $userdat['user_email'];
+        $this->URER_LOGO = $userdat['user_logo'];
+
+    }
+
+    public function userlog($user_id, $action='', $activity=0) {
+        $this->useractivity_model->addactivity($user_id, $action, $activity);
+    }
+
+    public function ajaxResponse($mdata, $merrors = array())
+    {
+        $aResponse = array(
+            'data' => $mdata,
+            'errors' => $merrors
+        );
+        echo(json_encode($aResponse));
+        exit;
+    }
+
+    public function isAjax()
+    {
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+        {
+            return TRUE;
+        }
+
+        return FALSE;
+    }
+
+
+}
--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -1,7 +1,38 @@
-<?php
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
 /**
- * Created by PhpStorm.
- * User: german
- * Date: 23.12.19
- * Time: 23:25
+ * Class My_Model
+ *
+ * @property Template template
+ * @property User_model user_model
+ * @property Useractivity_model useractivity_model
+ * @property Dashboard_model dashboard_model
+ * @property Menuitems_model menuitems_model
+ * @property Suppliers_model suppliers_model
+ * @property Items_model items_model
+ * @property Sections_model sections_model
+ * @property Categories_model categories_model
  */
+
+class MY_Model extends CI_Model
+{
+    public $success_result =1;
+    public $error_result = 0;
+
+    public $websystem='System';
+    public $websystem_id=1;
+
+
+    public function __construct()
+    {
+        date_default_timezone_set('America/New_York');
+        parent::__construct();
+    }
+
+    public function userlog($user_id, $action='', $activity = 0) {
+        $this->load->model('useractivity_model');
+        $this->useractivity_model->addactivity($user_id, $action, $activity);
+    }
+
+}
+?>
