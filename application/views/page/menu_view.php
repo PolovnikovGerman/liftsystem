72a1b8607a40c965bc3083c4c8d37622a7098d7c
German Polovnikov <polovnikov.g@gmail.com>
2020-01-14 19:59:05 +0200

    Content management

--- application/views/page/menu_view.php
+++ application/views/page/menu_view.php
@@ -1,5 +1,5 @@
 <?php foreach ($permissions as $item) { ?>
-    <?php if ($item['item_link']==$activelnk) { ?>
+    <?php if (!empty($item['item_link']) && $item['item_link']==$activelnk) { ?>
         <div class="activemenubutton <?=$item['menu_section']?>" data-menulink="<?=$item['item_link']?>"><?=$item['item_name']?></div>
     <?php } else { ?>
         <div class="menubutton <?=$item['menu_section']?>" data-menulink="<?=$item['item_link']?>"><?=$item['item_name']?></div>
