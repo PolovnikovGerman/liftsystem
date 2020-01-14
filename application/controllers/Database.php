cb9a3568db0d31dd2d7ce2fba60b5d7cff52e7f6
German Polovnikov <polovnikov.g@gmail.com>
2020-01-14 14:03:26 +0200

    Database management

--- application/controllers/Database.php
+++ application/controllers/Database.php
@@ -1,7 +1,62 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: german
- * Date: 14.01.20
- * Time: 13:56
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Database extends MY_Controller
+{
+
+    private $pagelink = '/database';
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        $pagedat = $this->menuitems_model->get_menuitem($this->pagelink);
+        if ($pagedat['result'] == $this->error_result) {
+            show_404();
+        }
+        $page = $pagedat['menuitem'];
+        $permdat = $this->menuitems_model->get_menuitem_userpermisiion($this->USR_ID, $page['menu_item_id']);
+        if ($permdat['result'] == $this->success_result && $permdat['permission'] > 0) {
+        } else {
+            if ($this->isAjax()) {
+                $this->ajaxResponse(array('url' => '/'), 'Your have no permission to this page');
+            } else {
+                redirect('/');
+            }
+        }
+    }
+
+    public function index()
+    {
+        $head = [];
+        $head['title'] = 'Database';
+        $menu = $this->menuitems_model->get_itemsubmenu($this->USR_ID, $this->pagelink);
+        $content_options = [];
+        foreach ($menu as $row) {
+            if ($row['item_link'] == '#customshappedview') {
+                // Custom shaped
+                $head['styles'][] = array('style' => '/css/content/customshape_page.css');
+                $head['scripts'][] = array('src' => '/js/content/custom_shaped.js');
+            }
+        }
+        $content_options['menu'] = $menu;
+        $content_view = $this->load->view('database/page_view', $content_options, TRUE);
+        // Add main page management
+        $head['scripts'][] = array('src' => '/js/database/page.js');
+        $head['styles'][] = array('style' => '/css/database/databasepage.css');
+        // Utils
+
+        $head['styles'][] = array('style' => '/css/page_view/pagination_shop.css');
+        $head['scripts'][] = array('src' => '/js/adminpage/jquery.mypagination.js');
+        $head['scripts'][] = array('src' => '/js/adminpage/fileuploader.js');
+        $head['styles'][] = array('style' => '/css/page_view/fileuploader.css');
+        $head['scripts'][] = array('src' => '/js/fancybox/jquery.fancybox.js');
+        $head['styles'][] = array('style' => '/css/fancybox/jquery.fancybox.css');
+
+        $options = ['title' => $head['title'], 'user_id' => $this->USR_ID, 'user_name' => $this->USER_NAME, 'activelnk' => $this->pagelink, 'styles' => $head['styles'], 'scripts' => $head['scripts'],];
+        $dat = $this->template->prepare_pagecontent($options);
+        $dat['content_view'] = $content_view;
+        $this->load->view('page/page_template_view', $dat);
+    }
+
+}
--- application/views/database/page_view.php
+++ application/views/database/page_view.php
@@ -1,7 +1,34 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: german
- * Date: 14.01.20
- * Time: 13:59
- */
+<div class="maincontent">
+    <div class="maincontentmenuarea databasemenu">
+        <div class="maincontentmenu">
+            <?php foreach ($menu as $item) { ?>
+                <div class="maincontentmenu_item" data-link="<?=str_replace('#','', $item['item_link'])?>"><?=$item['item_name']?></div>
+            <?php } ?>
+        </div>
+    </div>
+    <div class="maincontent_view">
+
+        <?php if (isset($taskview)) { ?>
+            <div class="artcontentarea" id="taskview" style="display: none;"><?=$taskview?></div>
+        <?php } ?>
+        <?php if (isset($orderlist)) { ?>
+            <div class="artcontentarea" id="orderlist" style="display: none;"><?=$orderlist?></div>
+        <?php } ?>
+        <?php if (isset($requestlist)) { ?>
+            <div class="artcontentarea" id="requestlist" style="display: none;"><?=$requestlist?></div>
+        <?php } ?>
+    </div>
+</div>
+
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="artModalLabel">New message</h4>
+            </div>
+            <div class="modal-body" style="float: left;"></div>
+            <div class="modal-footer"></div>
+        </div>
+    </div>
+</div>
