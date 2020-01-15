051ef3379b218f92269affc34de014c67f696a1d
German <polovnikov.g@gmail.com>
2020-01-15 17:49:20 +0200

    Database management (Items Categories page export)

---
+++ application/views/database/dbcategory_data_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polovnikov-go
+ * Date: 1/15/20
+ * Time: 3:48 PM
+ */
---
+++ application/views/database/dbcategory_table_data_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polovnikov-go
+ * Date: 1/15/20
+ * Time: 5:37 PM
+ */
