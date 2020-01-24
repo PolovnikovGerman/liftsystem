bfebfd2bb066320500cca7201506edc0ebd86af4
German Polovnikov <polovnikov.g@gmail.com>
2020-01-24 11:05:39 +0200

    Item details (Edit)

---
+++ application/views/itemdetails/metaedit_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 23.01.20
+ * Time: 19:56
+ */
---
+++ application/views/itemdetails/stressball_itempriceedit_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 23.01.20
+ * Time: 20:44
+ */
