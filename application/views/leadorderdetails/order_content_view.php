b1fe654edf8a907c1d784c77106650776ba6f0bf
German Polovnikov <polovnikov.g@gmail.com>
2020-01-29 13:26:34 +0200

    Order Edit

---
+++ application/views/leadorderdetails/order_content_view.php
@@ -0,0 +1,14 @@
+<div class="line_2">
+    <div class="line_3">
+        <?=$order?>
+    </div>
+    <div class="line_4">
+        <?=$messages_view?>
+        <div id="leadorderprofitarea">
+            <?=$profit_view?>
+        </div>        
+    </div>
+    <div class="block_7">
+        <?=$artview?>
+    </div>
+</div>
