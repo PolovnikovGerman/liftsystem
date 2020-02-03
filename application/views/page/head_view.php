db06a1830b00148f8cd4e0cb6e8a205714bed412
German Polovnikov <polovnikov.g@gmail.com>
2020-02-03 21:27:56 +0200

    Add qTip plugin

--- application/views/page/head_view.php
+++ application/views/page/head_view.php
@@ -4,6 +4,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="/css/page_view/main.css">
+<link rel="stylesheet" href="/css/page_view/jquery.qtip.css">
 <?php foreach ($styles as $row) { ?>
     <link rel="stylesheet" href="<?=$row['style']?>?v=<?=getsitecssversion()?>">
 <?php } ?>
@@ -11,6 +12,7 @@
 <script src="http://code.jquery.com/jquery-migrate-1.2.1.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 <script src="/js/adminpage/page.js"></script>
+<script src="/js/adminpage/jquery.qtip.js"></script>
 <?php foreach ($scripts as $row) { ?>
     <script src="<?=$row['src']?>?v=<?=getsitejsversion()?>"></script>
 <?php } ?>
