36089b9ec7b91697c51f2b9df7ab3169a0fedad4
German Polovnikov <polovnikov.g@gmail.com>
2020-02-05 01:35:16 +0200

    Analytics - Checkout report (month)

---
+++ application/views/reports/checkout_footer_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 04.02.20
+ * Time: 22:24
+ */
---
+++ application/views/reports/checkout_reportdata_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 05.02.20
+ * Time: 1:15
+ */
