9b1c40f860a7830df31da891707bd2496d840361
German Polovnikov <polovnikov.g@gmail.com>
2020-02-24 11:00:02 +0200

    Orders (Online list)

---
+++ application/views/orders/onlineorders_data_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 23.02.20
+ * Time: 10:55
+ */
---
+++ application/views/orders/onlineorders_head_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 23.02.20
+ * Time: 0:26
+ */
