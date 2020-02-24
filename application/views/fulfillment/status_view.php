3b76e7a14a962f962f85e38611dde4d5ec5a9bd5
German Polovnikov <polovnikov.g@gmail.com>
2020-02-25 01:57:12 +0200

    Order Status View

---
+++ application/views/fulfillment/status_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 24.02.20
+ * Time: 23:19
+ */
---
+++ application/views/fulfillment/statut_row_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 25.02.20
+ * Time: 1:38
+ */
