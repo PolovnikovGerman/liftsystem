404d159cc7e9b357bc638eaab453c6df89a91281
German Polovnikov <polovnikov.g@gmail.com>
2020-03-06 17:10:16 +0200

    Accounting (Open Invoices)

---
+++ application/views/finopenivoice/monitor_search_view.php
@@ -0,0 +1,13 @@
+<div class="monitorsearch_form">
+    <div class="monitorsearch_input">
+        <input id="monitorsearch" name="monitorsearch" class="monitorsearch_input" value="Enter order #, customer"/>
+    </div>    
+    <div class="search_action">
+        <a class="find_it" id="find_ord" href="javascript:void(0);">
+            Search It
+        </a>
+        <a class="find_it" id="clear_ord" href="javascript:void(0);">
+            Clear
+        </a>    
+    </div>
+</div>
