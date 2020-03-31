3ef58e41b8e050e6a780c80ae3ea184b6e6a36f6
German Polovnikov <polovnikov.g@gmail.com>
2020-03-31 22:17:43 +0300

    ART stages (daily report)

---
+++ application/views/messages/artorder_list_view.php
@@ -0,0 +1,12 @@
+<div style="clear: both; float: left; width: 900px;">
+    <div style="clear:both; float: left; width: 850px; text-align: center; font-size: 16px;">
+        Daily report - orders in project stage <?=date('m/d/Y H:i:s')?>
+    </div>
+    <?=$notplaced?>
+    <?=$notapprov?>
+    <?=$notprof?>
+    <?=$needprof?>
+    <?=$notvector?>
+    <?=$notredr?>
+    <?=$noart?>
+</div>
