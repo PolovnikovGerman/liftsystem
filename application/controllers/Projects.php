9848cf2d5ca977a901b7d5d85534a42bbccc7322
German Polovnikov <polovnikov.g@gmail.com>
2020-05-29 20:45:09 +0300

    Order Profit page

---
+++ application/controllers/Projects.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 29.05.20
+ * Time: 19:39
+ */
+
+class Projects
+{
+
+}
---
+++ application/controllers/Resources.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 29.05.20
+ * Time: 19:43
+ */
+
+class Resources
+{
+
+}
