64e74401aea8484b455847844bf73c8cb6e04cd6
German Polovnikov <polovnikov.g@gmail.com>
2020-05-29 22:56:22 +0300

    Order Profit page

--- application/controllers/Customservice.php
+++ application/controllers/Customservice.php
@@ -1,12 +1,56 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: german
- * Date: 29.05.20
- * Time: 22:55
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Customservice
+class Customservice extends MY_Controller
 {
 
+    private $pagelink = '/customservice';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $pagedat = $this->menuitems_model->get_menuitem($this->pagelink);
+        if ($pagedat['result'] == $this->error_result) {
+            show_404();
+        }
+        $page = $pagedat['menuitem'];
+        $permdat = $this->menuitems_model->get_menuitem_userpermisiion($this->USR_ID, $page['menu_item_id']);
+        if ($permdat['result'] == $this->success_result && $permdat['permission'] > 0) {
+        } else {
+            if ($this->isAjax()) {
+                $this->ajaxResponse(array('url' => '/'), 'Your have no permission to this page');
+            } else {
+                redirect('/');
+            }
+        }
+    }
+
+    public function index() {
+        $head = [];
+        $head['styles'] = $head['scripts'] = [];
+        $head['title'] = 'Accounting';
+        $menu = $this->menuitems_model->get_itemsubmenu($this->USR_ID, $this->pagelink);
+
+        $content_options = [];
+        foreach ($menu as $row) {
+
+        }
+        $content_options['menu'] = $menu;
+        $content_view = '';
+        // Add main page management
+
+        $options = [
+            'title' => $head['title'],
+            'user_id' => $this->USR_ID,
+            'user_name' => $this->USER_NAME,
+            'activelnk' => $this->pagelink,
+            'styles' => $head['styles'],
+            'scripts' => $head['scripts'],
+        ];
+
+        $dat = $this->template->prepare_pagecontent($options);
+        $dat['content_view'] = $content_view;
+        $this->load->view('page/page_template_view', $dat);
+
+    }
 }
