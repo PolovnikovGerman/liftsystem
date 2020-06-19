8ab13ec35e5a9e46e897de6f67de2a72a5b9a8d5
German Polovnikov <polovnikov.g@gmail.com>
2020-06-19 23:53:11 +0300

    Sign in

--- application/controllers/Leadorder.php
+++ application/controllers/Leadorder.php
@@ -1218,8 +1218,11 @@ class Leadorder extends MY_Controller
             $mdata=array();
             $error='';
             $postdata=$this->input->post();
-            firephplog($_POST,'DataPost');
             firephplog($postdata,'POST');
+            if (empty($postdata)) {
+                $postdata = $this->input->get();
+                firephplog($postdata,'POST AS GET');
+            }
             $ordersession=(isset($postdata['ordersession']) ? $postdata['ordersession'] : 0);
             $leadorder=usersession($ordersession);
             firephplog($leadorder,'data');
