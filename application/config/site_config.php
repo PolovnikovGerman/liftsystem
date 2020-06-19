c1c0f1b96f6237ec4430994c23eeb16eb17760ac
German Polovnikov <polovnikov.g@gmail.com>
2020-06-19 18:34:01 +0300

    Database content

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
 $config['js_version']='1.141';
-$config['css_version']='1.140';
+$config['css_version']='1.142';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
