1c6b6e8aa881a02cd85ab2c2ac9fbd0807e63b7d
German Polovnikov <polovnikov.g@gmail.com>
2020-06-20 01:23:54 +0300

    Database content

--- js/leads/leadorderpopup.js
+++ js/leads/leadorderpopup.js
@@ -1448,8 +1448,8 @@ function remove_leadartlocat(artloc) {
 
 function save_newleadlogoartloc(loctype) {
     var params=new Array();
-    // params.push({name: 'logo', value:$("input#filename").val()});
-    params.push({name: 'logo', value:'/uploads/preload/Nto24cKBjfu1.jpg'});
+    params.push({name: 'logo', value:$("input#filename").val()});
+    // params.push({name: 'logo', value:'/uploads/preload/Nto24cKBjfu1.jpg'});
     params.push({name: 'loctype', value: loctype});
     params.push({name: 'ordersession', value: $("input#ordersession").val()});
     // var url="/leadorder/artnewlocation_save";
