1f6de0a2443ff1bfb666e32745d8b83d4b86a810
German Polovnikov <polovnikov.g@gmail.com>
2020-06-22 00:25:01 +0300

    Bonus Report

--- application/controllers/Cronjob.php
+++ application/controllers/Cronjob.php
@@ -641,8 +641,7 @@ Class Cronjob extends CI_Controller
                 // $this->config->item('sage_email'),
                 // $this->config->item('taisenkatakura_email'),
                 // );
-                // $email_to=$this->config->item('sean_email');
-                $email_to='polovnikov.g@gmail.com';
+                $email_to=$this->config->item('sean_email');
                 // $email_cc=$this->config->item('developer_email');
                 $this->email->from($email_from);
                 $this->email->to($email_to);
@@ -673,12 +672,12 @@ Class Cronjob extends CI_Controller
             $config['wordwrap'] = TRUE;
             $this->email->initialize($config);
             $email_from=$this->config->item('email_notification_sender');
-            $email_to=$this->config->item('sean_email');
-            $email_cc=$this->config->item('sage_email');
-
+            // $email_to=$this->config->item('sean_email');
+            // $email_cc=$this->config->item('sage_email');
+            $email_to = 'polovnikov.g@gmail.com';
             $this->email->from($email_from);
             $this->email->to($email_to);
-            $this->email->cc($email_cc);
+            // $this->email->cc($email_cc);
             $title=date('D - M d, Y', $datestart).' - Sales Report (Shanequa Hall) (Owners version) ';
             if ($brand=='BT') {
                 $title.='(Bluetrack.com)';
