e311ebd946240abc35fc26b9a2af2b85b23745ed
German Polovnikov <polovnikov.g@gmail.com>
2020-07-01 23:12:55 +0300

    Lock China IPs

---
+++ application/models/Seo_model.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 01.07.20
+ * Time: 21:20
+ */
---
+++ application/views/errors/html/error_403.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: german
+ * Date: 01.07.20
+ * Time: 22:31
+ */
