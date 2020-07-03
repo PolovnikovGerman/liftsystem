609df69ef2ac24fdbf3e17fe64f0a942a9750b06
German Polovnikov <polovnikov.g@gmail.com>
2020-07-04 00:37:47 +0300

    Change logo for Invoice

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
 $config['js_version']='1.151';
-$config['css_version']='1.151';
+$config['css_version']='1.152';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
