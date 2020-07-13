0995bc15b5130ba07b98e922d5796c3cae79faff
German <polovnikov.g@gmail.com>
2020-07-14 00:14:36 +0300

    Main menu - drop downs

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='1.156';
-$config['css_version']='1.155';
+$config['js_version']='1.157';
+$config['css_version']='1.157';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
