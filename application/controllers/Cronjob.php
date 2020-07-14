f4312b717a6335294524c3bb087c4255ec87d364
German <polovnikov.g@gmail.com>
2020-07-14 18:24:51 +0300

    PO change notification

--- application/controllers/Cronjob.php
+++ application/controllers/Cronjob.php
@@ -467,8 +467,7 @@ Class Cronjob extends CI_Controller
             $config['wordwrap'] = TRUE;
             $this->email->initialize($config);
             $email_from=$this->config->item('email_notification_sender');
-            // $email_to=$this->config->item('sean_email');
-            $email_to = 'sean@houseofglasser.com';
+            $email_to=$this->config->item('sean_email');
             $email_cc=$this->config->item('sage_email');
             $this->email->from($email_from);
             $this->email->to($email_to);
