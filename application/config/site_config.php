43de4d3b8dbbdd032a601d0966c35f3953486ed1
German <polovnikov.g@gmail.com>
2020-10-24 00:08:47 +0300

    Main menu

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.0170';
-$config['css_version']='2.0170';
+$config['js_version']='2.0171';
+$config['css_version']='2.0171';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
