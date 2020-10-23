453b2166b207c313fa0f44ecc87b0fa4a02c0fe7
German <polovnikov.g@gmail.com>
2020-10-23 17:11:17 +0300

    Main menu

--- application/views/database/page_view.php
+++ application/views/database/page_view.php
@@ -1,35 +1,36 @@
 <div class="maincontent">
     <div class="maincontentmenuarea databasemenu">
         <div class="maincontentmenu">
+            <div class="title">Database:</div>
             <?php foreach ($menu as $item) { ?>
                 <div class="maincontentmenu_item <?=$start==str_replace('#','', $item['item_link']) ? 'active' : ''?>" data-link="<?=str_replace('#','', $item['item_link'])?>"><?=$item['item_name']?></div>
             <?php } ?>
         </div>
-    </div>
-    <?php if (isset($itempriceview)) { ?>
-        <div class="dbcontentarea" id="itempriceview" style="display: none;"><?=$itempriceview?></div>
-    <?php } ?>
-    <?php if (isset($itemcategoryview)) { ?>
-        <div class="dbcontentarea" id="itemcategoryview" style="display: none;"><?=$itemcategoryview?></div>
-    <?php } ?>
-    <?php if (isset($itemsequenceview)) { ?>
-        <div class="dbcontentarea" id="itemsequenceview" style="display: none;"><?=$itemsequenceview?></div>
-    <?php } ?>
-    <?php if (isset($itemmisinfoview)) { ?>
-        <div class="dbcontentarea" id="itemmisinfoview" style="display: none;"><?=$itemmisinfoview?></div>
-    <?php } ?>
-    <?php if (isset($itemprofitview)) { ?>
-        <div class="dbcontentarea" id="itemprofitview" style="display: none;"><?=$itemprofitview?></div>
-    <?php } ?>
-    <?php if (isset($itemtemplateview)) { ?>
-        <div class="dbcontentarea" id="itemtemplateview" style="display: none;"><?=$itemtemplateview?></div>
-    <?php } ?>
-    <?php if (isset($categoryview)) { ?>
-        <div class="dbcontentarea" id="categoryview" style="display: none;"><?=$categoryview?></div>
-    <?php } ?>
-    <div class="dbcontentarea" id="itemdetailsview" style="display: none;">
-        <div class="left_maincontent"></div>
-        <div class="right_maincontent"></div>
+        <?php if (isset($itempriceview)) { ?>
+            <div class="dbcontentarea" id="itempriceview" style="display: none;"><?=$itempriceview?></div>
+        <?php } ?>
+        <?php if (isset($itemcategoryview)) { ?>
+            <div class="dbcontentarea" id="itemcategoryview" style="display: none;"><?=$itemcategoryview?></div>
+        <?php } ?>
+        <?php if (isset($itemsequenceview)) { ?>
+            <div class="dbcontentarea" id="itemsequenceview" style="display: none;"><?=$itemsequenceview?></div>
+        <?php } ?>
+        <?php if (isset($itemmisinfoview)) { ?>
+            <div class="dbcontentarea" id="itemmisinfoview" style="display: none;"><?=$itemmisinfoview?></div>
+        <?php } ?>
+        <?php if (isset($itemprofitview)) { ?>
+            <div class="dbcontentarea" id="itemprofitview" style="display: none;"><?=$itemprofitview?></div>
+        <?php } ?>
+        <?php if (isset($itemtemplateview)) { ?>
+            <div class="dbcontentarea" id="itemtemplateview" style="display: none;"><?=$itemtemplateview?></div>
+        <?php } ?>
+        <?php if (isset($categoryview)) { ?>
+            <div class="dbcontentarea" id="categoryview" style="display: none;"><?=$categoryview?></div>
+        <?php } ?>
+        <div class="dbcontentarea" id="itemdetailsview" style="display: none;">
+            <div class="left_maincontent"></div>
+            <div class="right_maincontent"></div>
+        </div>
     </div>
 </div>
 
