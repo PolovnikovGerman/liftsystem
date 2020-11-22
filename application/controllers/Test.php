7556a25c19d1724fc5e128b8e2f6661ff421c1f6
German <polovnikov.g@gmail.com>
2020-11-22 17:40:41 +0200

    New Invoice

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,1 +1,42 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller
+{
+    public function index() {
+
+    }
+
+    public function testdoc() {
+        define('FPDF_FONTPATH', FCPATH.'font');
+        $this->load->library('fpdf/fpdf');
+        // Prepare
+        $logoFile = FCPATH."/img/invoice/invoice_logo_bluetrack-stressballs.jpg";
+        $logoXPos = 15;
+        $logoYPos = 15;
+        $logoWidth = 95;
+
+        $invnumImg = FCPATH.'/img/invoice/invoice_num.png';
+        $invnumXPos = 123;
+        $invnumYPos = 15;
+        $invnumWidth = 78;
+
+        $file = $this->config->item('upload_path_preload').'hello.pdf';
+        $pdf = new FPDF('P','mm','A4');
+        $pdf->AddPage();
+        $pdf->SetFont('Arial','',12);
+        $pdf->SetTextColor(65, 65, 65);
+        $pdf->SetMargins(14,14,14);
+        // Logo
+        $pdf->Image( $logoFile, $logoXPos, $logoYPos, $logoWidth );
+        // Inv #
+        $pdf->Image($invnumImg, $invnumXPos, $invnumYPos, $invnumWidth);
+        $pdf->SetXY(167,17);
+        $pdf->SetFont('Arial','B',18);
+        $pdf->SetTextColor(0, 0, 255);
+        $pdf->Cell(32,12,'MJ-42738',0,0,'C');
+
+        $pdf->Output('F', $file);
+    }
+
+}
