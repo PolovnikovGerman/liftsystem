ed18e380befb9d9530fe4024a32d8f3c86044fad
German <polovnikov.g@gmail.com>
2020-12-01 10:39:35 +0200

    PO #

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01790';
-$config['css_version']='2.01790';
+$config['js_version']='2.01792';
+$config['css_version']='2.01792';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
