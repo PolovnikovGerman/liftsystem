82aaf885e5bd7509928f185fa48c2f9b6146fe5d
German <polovnikov.g@gmail.com>
2020-12-23 14:36:35 +0200

    Calendar issue

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01792';
-$config['css_version']='2.01792';
+$config['js_version']='2.01793';
+$config['css_version']='2.01793';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
