a8cb609c109bf91bd68bb271c87d8519a7288bd6
German <polovnikov.g@gmail.com>
2021-03-29 20:56:31 +0300

    Profit (date) - show week shipping total

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01800';
-$config['css_version']='2.01800';
+$config['js_version']='2.01801';
+$config['css_version']='2.01801';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
