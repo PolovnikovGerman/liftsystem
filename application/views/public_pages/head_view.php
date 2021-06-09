bff0dede0c314a44e3f080f558006ddfbf36a643
German <polovnikov.g@gmail.com>
2021-06-09 20:12:06 +0300

    Items list - Details - Edit mode

--- application/views/public_pages/head_view.php
+++ application/views/public_pages/head_view.php
@@ -2,6 +2,11 @@
 <title><?=$title?></title>
 <link rel="stylesheet" href="/css/page_view/public.css">
 <link rel="stylesheet" href="/css/page_view/bootstrap.min.css">
+<?php if (isset($styles) && is_array($styles)) { ?>
+    <?php foreach ($styles as $row) { ?>
+        <link rel="stylesheet" href="<?=$row['style']?>?v=<?=getsitecssversion()?>">
+    <?php } ?>
+<?php } ?>
 <script src="/js/jquery.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 <script src="/js/signin/signin.js?r="<?=getsitejsversion()?>></script>
