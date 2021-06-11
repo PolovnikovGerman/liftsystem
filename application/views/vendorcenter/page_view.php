7bc1e47f395a1c09023dc7718e9893b092a9b500
German <polovnikov.g@gmail.com>
2021-06-11 19:15:49 +0300

    Vendors Center - List

--- application/views/vendorcenter/page_view.php
+++ application/views/vendorcenter/page_view.php
@@ -41,7 +41,7 @@
             <div class="addnewvendor">+ add</div>
         </div>
         <div class="type">Type</div>
-        <div class="slug sortable" data-sortcell="vendor_slug">Vendor #</div>
+        <div class="slug sortable" data-sortcell="vendor_slug">Vend #</div>
         <div class="name sortable active" data-sortcell="vendor_name">Vendor Name  <div class="ascsort">&nbsp;</div></div>
         <div class="altname sortable" data-sortcell="alt_name">Alternate Name</div>
         <div class="asinumber sortable" data-sortcell="alt_name">ASI #</div>
