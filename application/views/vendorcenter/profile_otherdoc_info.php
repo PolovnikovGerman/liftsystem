d9bc493cd12c867b95b91bd3863ca64c6c4fb3fa
German <polovnikov.g@gmail.com>
2021-06-22 14:45:40 +0300

    Vendors Center - Details view

---
+++ application/views/vendorcenter/profile_otherdoc_info.php
@@ -0,0 +1,5 @@
+<?php if ($count==0) { ?>
+    <div class="vendorparam_value">Empty Documents List</div>
+<?php } else { ?>
+    <div class="vendorparam_value documentlist"><?=$count?> documents - click to view</div>
+<?php } ?>
