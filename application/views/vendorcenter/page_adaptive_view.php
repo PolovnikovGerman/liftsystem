ea4d85e76ad6a3910526fa5f408cb4292604abc2
German <polovnikov.g@gmail.com>
2021-10-13 20:02:53 +0300

    Lift - adaptive design

--- application/views/vendorcenter/page_adaptive_view.php
+++ application/views/vendorcenter/page_adaptive_view.php
@@ -40,7 +40,20 @@
                     <div class="totaldata"><?=QTYOutput($total)?> Records</div>
                 </div>
             </div>
-
         </div>
+        <div class="datatitle">
+            <div class="status">
+                <div class="addnewvendor">+ add</div>
+            </div>
+            <div class="type">Type</div>
+            <div class="slug sortable" data-sortcell="vendor_slug">Vend #</div>
+            <div class="name sortable active" data-sortcell="vendor_name">Vendor Name  <div class="ascsort">&nbsp;</div></div>
+            <div class="altname sortable" data-sortcell="alt_name">Alternate Name</div>
+            <div class="asinumber sortable" data-sortcell="alt_name">ASI #</div>
+            <div class="website">Website</div>
+            <div class="phone">Phone</div>
+            <div class="itemqty">Our Items</div>
+        </div>
+        <div class="dataarea" id="vendorinfo"></div>
     </div>
 </main>
