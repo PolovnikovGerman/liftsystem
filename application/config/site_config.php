a94a6f6ea39b988f7db46a12f33e3cfe20c71c5e
German <polovnikov.g@gmail.com>
2021-10-14 22:48:54 +0300

    Lift - adaptive design

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01825';
-$config['css_version']='2.01825';
+$config['js_version']='2.01826';
+$config['css_version']='2.01826';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
