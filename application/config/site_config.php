411389b825b3a042f17e281d61cb98ea209345fd
German <polovnikov.g@gmail.com>
2021-10-20 01:59:02 +0300

    Validate zip code for Orders

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.0182';
-$config['css_version']='2.0182';
+$config['js_version']='2.01832';
+$config['css_version']='2.01832';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
