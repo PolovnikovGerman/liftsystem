eb8051bffe767241b9adfe4a6878c76185f75019
German <polovnikov.g@gmail.com>
2021-10-22 21:24:38 +0300

    Vendor details - adaptive design

--- application/views/vendorcenter/pricedoc_full_view.php
+++ application/views/vendorcenter/pricedoc_full_view.php
@@ -1,15 +1,26 @@
-<div class="content-row">
-    <div class="pricedoc_icon" data-file="<?=$docs[0]['doc_url']?>" data-source="<?=$docs[0]['doc_name']?>">
-        <i class="fa fa-file-text-o" aria-hidden="true"></i>
+<div class="row content-row">
+    <div class="col-1">
+        <div class="pricedoc_icon" data-file="<?=$docs[0]['doc_url']?>" data-source="<?=$docs[0]['doc_name']?>">
+            <i class="fa fa-file-text-o" aria-hidden="true"></i>
+        </div>
     </div>
-    <div class="pricedoc_label">Pricing Sheet</div>
-    <div class="priceyear_label">Year</div>
-    <div class="pricedoc_year"><?=$docs[0]['doc_year']?></div>
+    <div class="col-6 pr-0">
+        <div class="pricedoc_label">Pricing Sheet</div>
+    </div>
+    <div class="col-2">
+        <div class="priceyear_label">Year</div>
+    </div>
+    <div class="col-2">
+        <div class="pricedoc_year"><?=$docs[0]['doc_year']?></div>
+    </div>
 </div>
 <?php if ($count > 1) { ?>
-    <div class="content-row">
-        <div class="pricedocs_view">View historic pricing sheets</div>
+    <div class="row content-row">
+        <div class="col-12">
+            <div class="pricedocs_view">View historic pricing sheets</div>
+        </div>
     </div>
+
     <div class="historicpricingarea">
         <?php for($i=1; $i<=$listcnt; $i++) { ?>
             <div class="doccontent_row <?=$i%2==0 ? 'greydatarow' : 'whitedatarow'?>">
