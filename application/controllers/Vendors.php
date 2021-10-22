ce0233c6d561679565d729fec110328ecbc32a6f
German <polovnikov.g@gmail.com>
2021-10-22 17:40:16 +0300

    Vendor details - adaptive design

--- application/controllers/Vendors.php
+++ application/controllers/Vendors.php
@@ -389,8 +389,8 @@ class Vendors extends MY_Controller
                     $mdata['mobheader'] = $this->load->view('vendorcenter/mobile_header_view', $profile_options, TRUE);
                     $general_info = $this->load->view('vendorcenter/profilepage_general_info', $profile_options, TRUE);
                     $purchase_info = $this->load->view('vendorcenter/profilepage_purchase_info', $profile_options, TRUE);
-                    $pament_info =  $this->load->view('vendorcenter/profile_payment_info', $profile_options, TRUE);
-                    $prices_info = $this->load->view('vendorcenter/profile_prices_info', $profile_options, TRUE);
+                    $pament_info =  $this->load->view('vendorcenter/profilepage_payment_info', $profile_options, TRUE);
+                    $prices_info = $this->load->view('vendorcenter/profilepage_prices_info', $profile_options, TRUE);
                     $customer_info = $this->load->view('vendorcenter/profile_customers_info', $profile_options, TRUE);
                     $document_info = $this->load->view('vendorcenter/profile_otherdoc_info', $profile_options, TRUE);
 
