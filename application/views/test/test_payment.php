e78782667b08687160724e39952f178e64889f28
German <polovnikov.g@gmail.com>
2021-10-31 16:56:33 +0200

    Test payment

--- application/views/test/test_payment.php
+++ application/views/test/test_payment.php
@@ -1,50 +1,73 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"
+      style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
 <head>
     <meta name="viewport" content="width=device-width"/>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <title>Account Löschung jetztnachhilfe de</title>
+    <title>Welcome</title>
     <style type="text/css">
         img {
             max-width: 100%;
         }
+
         body {
-            -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6;
+            -webkit-font-smoothing: antialiased;
+            -webkit-text-size-adjust: none;
+            width: 100% !important;
+            height: 100%;
+            line-height: 1.6;
         }
+
         body {
             background-color: #f6f6f6;
         }
-        @media  only screen and (max-width: 640px) {
+
+        @media only screen and (max-width: 640px) {
             h1 {
-                font-weight: 600 !important; margin: 20px 0 5px !important;
+                font-weight: 600 !important;
+                margin: 20px 0 5px !important;
             }
+
             h2 {
-                font-weight: 600 !important; margin: 20px 0 5px !important;
+                font-weight: 600 !important;
+                margin: 20px 0 5px !important;
             }
+
             h3 {
-                font-weight: 600 !important; margin: 20px 0 5px !important;
+                font-weight: 600 !important;
+                margin: 20px 0 5px !important;
             }
+
             h4 {
-                font-weight: 600 !important; margin: 20px 0 5px !important;
+                font-weight: 600 !important;
+                margin: 20px 0 5px !important;
             }
+
             h1 {
                 font-size: 22px !important;
             }
+
             h2 {
                 font-size: 18px !important;
             }
+
             h3 {
                 font-size: 16px !important;
             }
+
             .container {
                 width: 100% !important;
             }
+
             .content {
                 padding: 10px !important;
             }
+
             .content-wrapper {
                 padding: 10px !important;
             }
+
             .invoice {
                 width: 100% !important;
             }
@@ -52,91 +75,92 @@
     </style>
 </head>
 <body style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6; background: #f6f6f6; margin: 0; padding: 0;">
-<table style="background: #f6f6f6; font-size: 14px;">
-    <tr>
-        <td width="10px">&nbsp;</td>
-        <td width="700" style="padding-top: 15px">
-            <table style="background: #757575; width: 100%;">
-                <tr>
-                    <td style="width: 2%;">&nbsp;</td>
-                    <td style="width: 94%; background: #757575; text-align: center;">
-                        <table style="background: #757575; width: 100%;">
-                            <tr>
-                                <td style="width: 100%; text-align: center; vertical-align: middle; padding: 1.2rem 0;">
-                                    <div style="width: 60%;margin: 0 auto;">
-                                        <div style="width: 100%;float: left;">
-                                            <img src="https://jetztnachhilfe.de/img/message/logo_jetztnachhilfe.png" alt="Logo" style="max-width: 100%">
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                    <td style="width: 2%">&nbsp;</td>
-                </tr>
-                <tr>
-                    <td style="width: 2%">&nbsp;</td>
-                    <td style="width: 94%; background: #f6f6f6; padding: 2%;">
-                        <!-- Body table -->
-                        <table class="main" width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background: #fff; margin: 0; padding: 0; border: 1px solid #e9e9e9;">
-                            <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
-                                <td class="content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 20px; font-weight:bold; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                                    Hallo Herman <!-- {{name}}! -->
-                                </td>
-                            </tr>
-                            <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
-                                <td class="content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                                    Danke, dass du dich für jetztnachhilfe.de entschieden hast! Durch deinen Kauf hast du ab jetzt 30 Tage Zugriff auf über 10.000 Nachhilfelehrer. Nach 30 Tagen endet deine Mitgliedschaft automatisch. Deine Rechnung erhältst du mit dieser Mail.<br><br>
-                                    Du kannst nun alle Tutoren kontaktieren und so den richtigen Nachhilfelehrer für dich finden. Zusammen mit unseren Tutoren kannst du daran arbeiten deine schulischen Ziele zu erreichen. Wir wünschen dir viel Erfolg bei der Nachhilfelehrer Suche!<br><br>
-                                    Anbei findest du deine Rechung.<br><br>
-                                    Bei Fragen kannst du uns gerne unter info@jetztnachhilfe.de kontaktieren oder unter 0800 1111236 anrufen.<br><br>
-                                    Viele Grüße,<br><br>
-                                    Dein jetztnachhilfe Team<br>
-                                </td>
-                            </tr>
-                        </table>
-                        <!-- End Body Table -->
-                    </td>
-                    <td style="width: 2%;">&nbsp;</td>
-                </tr>
-            </table>
-            <table style="background: #757575; width: 100%; color: #ffffff">
-                <tr>
-                    <td style="text-align: center;">
-                        <img src="https://jetztnachhilfe.de/img/message/email_footer_3.png"/>
-                    </td>
-                </tr>
-                <!-- Desktop Contacts -->
-                <tr>
-                    <td style="padding-left: 2%;">&copy; 2021 Studienkreis GmbH. Alle Rechte vorbehalten.</td>
-                </tr>
-                <tr style="height: 28px;line-height: 28px;">
-                    <td style="padding-left: 2%;">
-                        <a style="color: #ffffff; text-decoration: none; font-weight: 600;" href="https://jetztnachhilfe.de/allgemeine-gesch%C3%A4ftsbedingungen">AGB</a> |
-                        <a style="color: #ffffff; text-decoration: none; font-weight: 600;" href="https://jetztnachhilfe.de/datenschutz">Datenschutz</a>  |
-                        <a style="color: #ffffff; text-decoration: none; font-weight: 600;" href="https://jetztnachhilfe.de/impressum">Impressum</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding-left: 2%;">
-                        jetztnachhilfe de Teil der Studienkreis GmbH | Universitätsstraße 104 | 44799 Bochum
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding-left: 2%;">
-                        Geschäftsführer: Lorenz Haase
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding-left: 2%;">
-                        Registergericht: HRB 4581 | USt-IdNr.: DE124086608
-                    </td>
-                </tr>
-            </table>
+<table class="body-wrap"
+       style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background: #f6f6f6; margin: 0; padding: 0;">
+    <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+        <td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0;"
+            valign="top"></td>
+        <td class="container" width="600"
+            style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto; padding: 0;"
+            valign="top">
+            <div class="content"
+                 style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;">
+
+                <img src="https://www.tutoria.de/img/email-footer-1.png" alt="">
+                <table class="main" width="100%" cellpadding="0" cellspacing="0"
+                       style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background: #fff; margin: 0; padding: 0; border: 1px solid #e9e9e9;">
+                    <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                        <td class="content-wrap"
+                            style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 20px;"
+                            valign="top">
+                            <table width="100%" cellpadding="0" cellspacing="0"
+                                   style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                    <td class="content-block"
+                                        style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 20px; font-weight:bold; vertical-align: top; margin: 0; padding: 0 0 20px;"
+                                        valign="top">
+                                        Hallo {{name}}!
+                                    </td>
+                                </tr>
+                                <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                    <td class="content-block"
+                                        style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
+                                        valign="top">
+                                        Vielen Dank für deine Registrierung bei tutoria!<br>
+                                        <br>
+                                        Wir freuen uns, dich bei tutoria begrüßen zu können.
+                                    </td>
+                                </tr>
+                                <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                    <td class="content-block"
+                                        style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
+                                        valign="top">
+                                        Du musst nur noch eine Sache machen bevor du unsere Nachhilfelehrer erforschen
+                                        kannst. Bitte bestätige deine E-Mail-Adresse indem du auf diesen Button oder auf
+                                        den Link klickst:<br>
+                                        <br>
+                                    </td>
+                                </tr>
+                                <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                    <td class="content-block"
+                                        style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
+                                        valign="top">
+                                        <a href="{{link}}" class="btn-primary"
+                                           style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; color: #FFF; text-decoration: none; line-height: 2; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px;  background: #348eda; margin: 0; padding: 0; border-color: #348eda; border-style: solid; border-width: 10px 20px;">E-Mail
+                                            bestätigen</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>{{link}}
+                                    <td>
+                                </tr>
+                                <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                    <td class="content-block"
+                                        style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
+                                        valign="top">
+                                        Jetzt steht deinem Erfolg bei tutoria nichts mehr im Weg!<br>
+                                        Falls du noch Fragen hast kannst du uns gerne unter info@tutoria.de oder 0800
+                                        111 12 36 kontaktieren!<br>
+                                        Dein tutoria Team
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <img src="https://www.tutoria.de/img/email-footer-2.png" alt="">
+                <div class="footer"
+                     style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; clear: both; color: #999; margin: 0; padding: 20px;">
+                    <table width="100%"
+                           style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+
+                    </table>
+                </div>
+            </div>
         </td>
-        <td width="10px">&nbsp;</td>
+        <td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0;"
+            valign="top"></td>
     </tr>
 </table>
-
 </body>
 </html>
