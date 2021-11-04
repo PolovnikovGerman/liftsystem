efdcf9e8c88fb6634bfa54d27fb2137300d3ad71
German <polovnikov.g@gmail.com>
2021-11-04 18:05:18 +0200

    DB Center - Vendors

--- application/views/vendorcenter/profile_adaptive_view.php
+++ application/views/vendorcenter/profile_adaptive_view.php
@@ -1,20 +1,20 @@
 <div class="row">
-    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 pr-0">
         <div class="vendordetails-section">
             <?=$general_view?>
         </div>
     </div>
-    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 pr-0">
         <div class="vendordetails-section">
             <?=$purchase_view?>
         </div>
     </div>
-    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3">
+    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 pr-0">
         <div class="vendordetails-section">
             <?=$payment_view?>
         </div>
     </div>
-    <div class="col-12 col-sm-6 col-md-6 col-lg-12 col-xl-3">
+    <div class="col-12 col-sm-6 col-md-6 col-lg-12 col-xl-3 pr-0">
         <div class="row">
             <div class="col-12 col-lg-4 col-xl-12">
                 <div class="vendordetails-section pricesview">
