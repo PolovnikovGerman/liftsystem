f5235ee597351075e464b103141ff417af407b41
German <polovnikov.g@gmail.com>
2021-11-10 22:06:18 +0200

    DB Center - Content

--- application/views/content/mob_submenu_view.php
+++ application/views/content/mob_submenu_view.php
@@ -2,11 +2,13 @@
 <?php foreach ($menus as $menu) { ?>
     <div class="col-3 mb-2">
         <div class="submenu_item" data-link="<?=str_replace('#', '', $menu['item_link'])?>" data-brand="<?=$brand?>"><?=$menu['item_name']?></div>
-        <div class="row">
-            <div class="submenu_manage" data-link="<?=str_replace('#', '', $menu['item_link'])?>" data-brand="<?=$brand?>">
+        <div class="submenu_manage" data-link="<?=str_replace('#', '', $menu['item_link'])?>" data-brand="<?=$brand?>">
+            <div class="row">
                 <div class="col-12">
                     <div class="submenu_label">View Mode</div>
                 </div>
+            </div>
+            <div class="row">
                 <div class="col-12">
                     <div class="buttons">
                         <div class="edit_button" data-link="<?=str_replace(['#',strtolower($brand)], '', $menu['item_link'])?>" data-brand="<?=$brand?>">
