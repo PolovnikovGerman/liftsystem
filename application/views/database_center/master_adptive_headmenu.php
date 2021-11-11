1866c73a18d8e6d2f21df6b2cd5d9617c71c5608
German <polovnikov.g@gmail.com>
2021-11-11 22:18:43 +0200

    DB Center - Vendors

--- application/views/database_center/master_adptive_headmenu.php
+++ application/views/database_center/master_adptive_headmenu.php
@@ -1,13 +1,17 @@
 <main class="container-fluid">
     <div class="mastermenu_head">
         <div class="row">
-            <div class="col-12 col-sm-3 col-md-3 col-lg-2 col-xl-2 menulabel">MASTER</div>
-            <div class="col-sm-3 col-md-3 col-lg-2 col-xl-4 smallmobile">
-                <div class="returndbcenter">
-                    <img src="/img/database_center/return_to_mainscreen.png" class="img-responsive img-fluid" alt="'Back Main Screen"/>
+            <div class="col-12 col-sm-3 col-md-3 col-lg-2 col-xl-2">
+                <div class="menulabel">
+                    MASTER
+                </div>
+                <div class="smallmobile">
+                    <div class="returndbcenter">
+                        <img src="/img/database_center/return_to_mainscreen.png" class="img-responsive img-fluid" alt="'Back Main Screen"/>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-sm-6 col-md-6 col-lg-8 col-xl-6">
+            <div class="col-12 col-sm-7 col-md-6 col-lg-8 col-xl-6">
                 <div class="row">
                     <?php foreach ($menu as $mrow) { ?>
                         <div class="col-6 col-sm-6 col-md-6 col-lg-3 col-xl-3">
@@ -18,7 +22,7 @@
                     <?php } ?>
                 </div>
             </div>
-            <div class="col-sm-3 col-md-3 col-lg-2 col-xl-4 bigdeviceview">
+            <div class="col-sm-2 col-md-3 col-lg-2 col-xl-4 bigdeviceview">
                 <div class="returndbcenter">
                     <img src="/img/database_center/return_to_mainscreen.png" class="img-responsive img-fluid" alt="'Back Main Screen"/>
                 </div>
