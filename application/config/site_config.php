c966f81365a7e505ceb770bab03eb0cb91d4398d
German <polovnikov.g@gmail.com>
2021-11-12 11:08:31 +0200

    DB Center - Vendors

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01849';
-$config['css_version']='2.01849';
+$config['js_version']='2.01850';
+$config['css_version']='2.01850';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
