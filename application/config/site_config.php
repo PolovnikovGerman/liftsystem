8184a1a2f5a837d0f7b84ad523296ed904ffe40c
German <polovnikov.g@gmail.com>
2021-12-19 13:44:00 +0200

    Print shop inventory

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01853';
-$config['css_version']='2.01853';
+$config['js_version']='2.01854';
+$config['css_version']='2.01854';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
