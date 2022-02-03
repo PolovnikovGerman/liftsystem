8674e30e286c9acd488a9dcced24e470beaa7af7
German <polovnikov.g@gmail.com>
2022-02-03 15:57:30 +0200

    Profit (by orders)

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01859';
-$config['css_version']='2.01859';
+$config['js_version']='2.01860';
+$config['css_version']='2.01860';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
