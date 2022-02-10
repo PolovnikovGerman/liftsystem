eeb1326ba65bb2b5b90e04b0e096955ce9af80f3
German <polovnikov.g@gmail.com>
2022-02-10 20:22:12 +0200

    Finance - Accounts Receivable

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01866';
-$config['css_version']='2.01866';
+$config['js_version']='2.01867';
+$config['css_version']='2.01867';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
