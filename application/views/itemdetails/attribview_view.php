66cb849f58103d87cc8bf9b1ea8d62feeafd7300
German <polovnikov.g@gmail.com>
2022-02-18 13:03:02 +0200

    Issue with htmlspecialchars

--- application/views/itemdetails/attribview_view.php
+++ application/views/itemdetails/attribview_view.php
@@ -3,14 +3,14 @@
         <?php if ($item_description1 == '') { ?>
             <div style="height:16px;width:230px;" class="empty_value">&nbsp;</div>
         <?php } else { ?>
-            <div class="tooltip-descript" data-content="<?= $item_description1 ?>"><?= $item_description1 ?></div>
+            <div class="tooltip-descript" data-content="<?=htmlspecialchars_decode($item_description1) ?>"><?=htmlspecialchars_decode($item_description1) ?></div>
         <?php } ?>
     </div>
     <div class="attributes_row">
         <?php if ($item_description2 == '') { ?>
             <div style="height:16px;width:230px;" class="empty_value">&nbsp;</div>
         <?php } else { ?>
-            <div class="tooltip-descript" data-content="<?= $item_description2 ?>"><?= $item_description2 ?></div>
+            <div class="tooltip-descript" data-content="<?=htmlspecialchars_decode($item_description2) ?>"><?=htmlspecialchars_decode($item_description2) ?></div>
         <?php } ?>
     </div>
 </div>
