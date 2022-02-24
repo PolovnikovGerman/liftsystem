bb151e4aa3104a18a8bca32c8ad7a8e6c5bad467
German <polovnikov.g@gmail.com>
2022-02-24 14:50:45 +0200

    Finance - Accounts Receivable (mobile version)

--- application/views/accreceiv/totalsrefund_device_view.php
+++ application/views/accreceiv/totalsrefund_device_view.php
@@ -1,8 +1,8 @@
 <div class="row">
     <div class="col-12 accreceiv-totalrefund">
         <div class="row">
-            <div class="col-6 col-sm-5 col-md-5 accreceiv-totalrefund-title">Refunds to Customers:</div>
-            <div class="col-5 col-sm-5 col-md-4 accreceiv-totalrefund-value">(<?=TotalOutput(abs($totalrefund))?>)</div>
+            <div class="col-6 col-sm-5 col-md-5 col-lg-6 accreceiv-totalrefund-title">Refunds to Customers:</div>
+            <div class="col-5 col-sm-5 col-md-4 col-lg-4 accreceiv-totalrefund-value">(<?=TotalOutput(abs($totalrefund))?>)</div>
         </div>
     </div>
 </div>
