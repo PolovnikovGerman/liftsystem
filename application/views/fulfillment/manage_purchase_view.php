ef9241c0ac27817e520d6bb264164ad33f3bc20d
German <polovnikov.g@gmail.com>
2022-02-27 16:33:08 +0200

    Manage Purchase methods

--- application/views/fulfillment/manage_purchase_view.php
+++ application/views/fulfillment/manage_purchase_view.php
@@ -1,1 +1,8 @@
-<?php
+<div class="manage-purchase-area">
+    <div class="datarow">
+        <input type="text" class="addnewpurchase"/>
+        <div class="addmethodbtn">add new</div>
+    </div>
+    <div class="datarow" id="activemethods"><?=$active?></div>
+    <div class="datarow" id="inactivemethods"><?=$inactive?></div>
+</div>
--- application/views/fulfillment/purchase_methods_view.php
+++ application/views/fulfillment/purchase_methods_view.php
@@ -1,1 +1,11 @@
-<?php
+<div class="methodslabel"><?=$active==1 ? 'ACTIVE:' : 'INACTIVE:'?></div>
+<div class="methodsdata">
+    <?php foreach ($methods as $method) { ?>
+        <div class="datarow">
+            <div class="purchmethodname"> - <?=$method['method_name']?></div>
+            <div class="purchmethodaction <?=$active==1 ? 'deactivate' : 'activate'?>" data-method="<?=$method['method_id']?>">
+                <?=$active==1 ? 'deactivate' : 'activate'?>
+            </div>
+        </div>
+    <?php } ?>
+</div>
