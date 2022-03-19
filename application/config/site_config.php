e078a51b9b602630379bfcf3de007b021ef30ea0
German <polovnikov.g@gmail.com>
2022-03-19 22:03:40 +0200

    PO totals - view

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01887';
-$config['css_version']='2.01887';
+$config['js_version']='2.01888';
+$config['css_version']='2.01888';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
