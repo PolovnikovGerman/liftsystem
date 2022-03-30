4d0693831ba5bb85c8e99d95196a0f05bb7d0817
German <polovnikov.g@gmail.com>
2022-03-30 17:03:53 +0300

    Finance - new head menu

--- application/views/page/menu_new_view.php
+++ application/views/page/menu_new_view.php
@@ -2,12 +2,16 @@
     <?php foreach ($permissions as $item) { ?>
         <?php if ($item['menu_section']=='finsection') { ?>
             <?php if ($item['item_name']=='Finance') { ?>
-                <div class="menuitem finance <?=$item['item_link'] == $activelnk ? 'activelink' : ''?>" data-menulink="<?= $item['item_link'] ?>">
-                    <span><?= $item['item_name'] ?></span>
+                <div class="menuitem finance <?=($activelnk=='/accounting' || $activelnk=='/finances') ? 'activelink' : ''?>" data-menulink="/accounting">
+                    <span class="bnt-side">&nbsp;</span>
+                    <span>Finance</span>
+                    <span class="triangle-top">&nbsp;</span>
                 </div>
             <?php } elseif ($item['item_name']=='Finance NEW') { ?>
-                <div class="menuitem financenew <?=$item['item_link'] == $activelnk ? 'activelink' : ''?>" data-menulink="<?= $item['item_link'] ?>">
-                    &nbsp;
+                <div class="menuitem financenew <?=($activelnk=='/accounting' || $activelnk=='/finances') ? 'activelink' : ''?>" data-menulink="/finances">
+                    <span class="triangle-bootom">&nbsp;</span>
+                    <span>Finance</span>
+                    <span class="bnt-side">&nbsp;</span>
                 </div>
             <?php } else { ?>
                 <div class="menuitem <?=$item['item_link'] == $activelnk ? 'activelink' : ''?> <?=ifset($item,'newver', 1)==0 ? 'oldver' :  ''?>" data-menulink="<?= $item['item_link'] ?>">
