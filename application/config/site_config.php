f8ee5225be4cb7037d7036e5dced8241bebf6c08
German <polovnikov.g@gmail.com>
2022-03-31 19:00:17 +0300

    Finance - new head menu

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01915';
-$config['css_version']='2.01915';
+$config['js_version']='2.01916';
+$config['css_version']='2.01916';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
