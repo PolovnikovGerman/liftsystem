cad4d0680e582bd7d433bbb8bf30bd30dc174b4e
German <polovnikov.g@gmail.com>
2022-04-06 10:11:09 +0300

    Master Inventory

--- application/views/masterinvent/page_adaptive_view.php
+++ application/views/masterinvent/page_adaptive_view.php
@@ -23,12 +23,12 @@
                 </div>
             </div>
             <div class="col-12 col-sm-6 col-md-3 col-lg-2 col-xl-2">
-                <div class="row mb-2">
-                    <div class="col-3 filterlabel">
+                <div class="row mb-2 mt-2">
+                    <div class="col-3 inventfilterlabel">
                         Display:
                     </div>
                     <div class="col-8">
-                        <select class="filterdata" name="filerdata" id="filterdata_desktop">
+                        <select class="inventfilterdata" name="inventfilerdata">
                             <option value="0" selected="selected">Active & Inactive</option>
                             <option value="1">Active</option>
                             <option value="2">Non-Active</option>
@@ -36,8 +36,8 @@
                     </div>
                 </div>
                 <div class="row mb-2">
-                    <div class="col-12 totalinvent">
-                        <div class="row">
+                    <div class="col-12">
+                        <div class="row  totalinvent">
                             <div class="col-6 totalinventlabel">Total Value:</div>
                             <div class="col-6 totalinventvalue">$1,300,392.00</div>
                         </div>
@@ -45,6 +45,140 @@
                 </div>
             </div>
         </div>
+        <div class="row mt-2">
+            <div class="col-12">
+                <div class="mastinvent_left_menu">
+                    <div class="mastinvent_left_section active">
+                        <div class="mastinvent_left_sectiondata active">
+                            <div class="inventsectionhead">SSB - Sellable Stress Balls</div>
+                            <div class="inventsectionvalue">
+                                <div class="inventsectionvalue_label">Value:</div>
+                                <div class="inventsectionvalue_data">$692,500.00</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mastinvent_left_section">
+                        <div class="mastinvent_left_sectiondata">
+                            <div class="inventsectionhead">SMA - Sellable Masks</div>
+                            <div class="inventsectionvalue">
+                                <div class="inventsectionvalue_label">Value:</div>
+                                <div class="inventsectionvalue_data">$692,500.00</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mastinvent_left_section">
+                        <div class="mastinvent_left_sectiondata">
+                            <div class="inventsectionhead">SHS - Sellable Hand Sanitizers</div>
+                            <div class="inventsectionvalue">
+                                <div class="inventsectionvalue_label">Value:</div>
+                                <div class="inventsectionvalue_data">$692,500.00</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mastinvent_left_section">
+                        <div class="mastinvent_left_sectiondata">
+                            <div class="inventsectionhead">SOT - Other Sellable Items</div>
+                            <div class="inventsectionvalue">
+                                <div class="inventsectionvalue_label">Value:</div>
+                                <div class="inventsectionvalue_data">$692,500.00</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mastinvent_left_section rawsbtype">
+                        <div class="mastinvent_left_sectiondata">
+                            <div class="inventsectionhead">RAW - Raw Materials</div>
+                            <div class="inventsectionvalue">
+                                <div class="inventsectionvalue_label">Value:</div>
+                                <div class="inventsectionvalue_data">$692,500.00</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mastinvent_left_section">
+                        <div class="mastinvent_left_sectiondata">
+                            <div class="inventsectionhead">SHP - Shipping Supplies</div>
+                            <div class="inventsectionvalue">
+                                <div class="inventsectionvalue_label">Value:</div>
+                                <div class="inventsectionvalue_data">$692,500.00</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mastinvent_left_section">
+                        <div class="mastinvent_left_sectiondata">
+                            <div class="inventsectionhead">PRT - Printing Supplies</div>
+                            <div class="inventsectionvalue">
+                                <div class="inventsectionvalue_label">Value:</div>
+                                <div class="inventsectionvalue_data">$692,500.00</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mastinvent_left_section">
+                        <div class="mastinvent_left_sectiondata">
+                            <div class="inventsectionhead">SUP - Other Supplies</div>
+                            <div class="inventsectionvalue">
+                                <div class="inventsectionvalue_label">Value:</div>
+                                <div class="inventsectionvalue_data">$692,500.00</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mastinvent_databody">
+                    <div class="row mt-2">
+                        <div class="col-12">
+                            <div class="masterinventlegend">
+                                <div class="legendmapicon severeval">
+                                    <i class="fa fa-square"></i>
+                                </div>
+                                <div class="legendmaplabel">Severe (25% & Under)</div>
+                                <div class="legendmapicon lowval">
+                                    <i class="fa fa-square"></i>
+                                </div>
+                                <div class="legendmaplabel">Low (26% - 50%)</div>
+                            </div>
+                            <div class="masterinventexport">
+                                <i class="fa fa-share-square-o" aria-hidden="true"></i>
+                                Export SSB Inventory
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="masterinventtotals">
+                                <div class="inventtotalempty">&nbsp;</div>
+                                <div class="inventtotalmaxshow">Show Max</div>
+                                <div class="inventtotalinstock">910,545</div>
+                                <div class="inventtotalavailable">910,545</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="masterinventtablehead">
+                                <div class="addnewmasterinvent">
+                                    <i class="fa fa-plus"></i>
+                                    <span>add</span>
+                                </div>
+                                <div class="masterinventseq">Seq</div>
+                                <div class="masterinvent">Master #</div>
+                                <div class="masterinvent">Description</div>
+                                <div class="masterinvent">%</div>
+                                <div class="masterinvent">In Stock</div>
+                                <div class="masterinvent">Reserved</div>
+                                <div class="masterinvent">Available</div>
+                                <div class="masterinvent">Unit</div>
+                                <div class="masterinvent">On Order</div>
+                                <div class="masterinvent">Avg Price</div>
+                                <div class="masterinvent">Total Value</div>
+                                <div class="masterinvent">Details</div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="masterinventtablebody">
 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
