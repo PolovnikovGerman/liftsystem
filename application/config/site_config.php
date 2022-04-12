1608da9b6ca1afb5cad7803a98ab680a949e0292
German <polovnikov.g@gmail.com>
2022-04-12 22:58:57 +0300

    Promo prices profit

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01923';
-$config['css_version']='2.01923';
+$config['js_version']='2.01924';
+$config['css_version']='2.01924';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
