fdbac6e5f5ee57a3474e1125067a23b04d8c9d2e
German <polovnikov.g@gmail.com>
2022-04-15 00:38:46 +0300

    Accounts receiviable

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.019241';
-$config['css_version']='2.019241';
+$config['js_version']='2.01925';
+$config['css_version']='2.01925';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
