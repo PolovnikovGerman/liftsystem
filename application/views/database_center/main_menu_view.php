66dc4b83aee1da29d6185182d097805d23cc40e5
German <polovnikov.g@gmail.com>
2022-05-29 18:58:08 +0300

    Lift Database Center

--- application/views/database_center/main_menu_view.php
+++ application/views/database_center/main_menu_view.php
@@ -1,1 +1,5 @@
-<?php
+<div class="dbcenter_main_menu">
+    <?php foreach ($menus as $menu) { ?>
+        <div class="dbcenter-main-button <?=$start==$menu['item_link'] ? 'active' : ''?>"  data-link="<?=$menu['item_link']?>"><?=$menu['item_name']?></div>
+    <?php } ?>
+</div>
--- js/database_center/main_page.js
+++ js/database_center/main_page.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+    // var url=$(".dbcenter-main-button").first().data('link');
+    // activate_dbcenter(url);
+    // $(".dbcenter-main-button").first().addClass('active');
+    $(".dbcenter-main-button").unbind('click').click(function () {
+        var url = $(this).data('link');
+        $(".dbcenter-main-button").removeClass('active');
+        $(this).addClass('active');
+        activate_dbcenter(url);
+    })
+});
+
+function activate_dbcenter(dburl) {
+    var params=new Array();
+    params.push({name: 'url', value: dburl});
+    var url = '/databasecenter/show_channel';
+    $.post(url, params, function (response) {
+        if (response.errors=='') {
+            $(".dbcenter_content").empty().html(response.data.content);
+            activate_channel_content();
+        } else {
+            show_error(response);
+        }
+    },'json');
+}
+
+function activate_channel_content() {
+
+}
+/*
+$(document).ready(function(){
     init_dbcenter_main();
 });
 
@@ -32,3 +62,4 @@ function init_dbcenter_main() {
 
     });
 }
+*/
