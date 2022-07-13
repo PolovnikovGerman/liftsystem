6ebe793294da233714f58d537d447dff6c60c75f
German <polovnikov.g@gmail.com>
2022-07-13 15:19:34 +0300

    Item Center - Stress Relevers

--- application/controllers/Dbitems.php
+++ application/controllers/Dbitems.php
@@ -317,6 +317,7 @@ class Dbitems extends MY_Controller
                     $vendor_main = $this->load->view('relieveritems/vendormain_view',[],TRUE);
                     $vendor_prices = $this->load->view('relieveritems/vendorprices_view',['vendor_prices' => $data['vendor_price'], 'venditem' => $data['vendor_item']],TRUE);
                     $itemprices = $this->load->view('relieveritems/itemprices_view',['item' => $data['item'],'prices'=> $data['prices']],TRUE);
+                    
                 }
                 $body_options = [
                     'keyinfo' => $keyinfo,
