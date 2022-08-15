a0636e497123499dd38e0df3935444a55f6a5c13
German <polovnikov.g@gmail.com>
2022-08-15 21:29:01 +0300

    Relievers Items

--- application/views/relieveritems/itemdetailsbody_view.php
+++ application/views/relieveritems/itemdetailsbody_view.php
@@ -1,17 +1,20 @@
-<div class="leftpartitembody">
-    <?=$keyinfo?>
-    <?=$similar?>
-</div>
-<div class="centralpartbody">
-    <?=$vendor_main?>
-    <?=$itemimages?>
-</div>
-<div class="rightpartbody">
-    <div class="pricesarea">
-        <?=$vendor_prices?>
-        <?=$itemprices?>
+<div class="itemdetails-tab active">Main Info</div>
+<div class="itemdetails-infoarea">
+    <div class="leftpartitembody">
+        <?=$keyinfo?>
+        <?=$similar?>
     </div>
-    <?=$customview?>
-    <?=$metaview?>
-    <div class="itemshippingarea"><?=$shipping?></div>
+    <div class="centralpartbody">
+        <?=$vendor_main?>
+        <?=$itemimages?>
+    </div>
+    <div class="rightpartbody">
+        <div class="pricesarea">
+            <?=$vendor_prices?>
+            <?=$itemprices?>
+        </div>
+        <?=$customview?>
+        <?=$metaview?>
+        <div class="itemshippingarea"><?=$shipping?></div>
+    </div>
 </div>
