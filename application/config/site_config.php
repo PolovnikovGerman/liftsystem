eb9b260c8572c5fafdeefda288803e22079579e2
German <polovnikov.g@gmail.com>
2022-08-18 13:51:21 +0300

    Inventory - mark container as arrived

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01965';
-$config['css_version']='2.01965';
+$config['js_version']='2.01966';
+$config['css_version']='2.01966';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
