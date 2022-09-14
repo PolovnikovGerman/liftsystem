8c3df90a4c7cce06080f6f14ea87b442707d28ec
German <polovnikov.g@gmail.com>
2022-09-14 23:46:16 +0300

    Relievers Items - Imprint Places

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -49,6 +49,7 @@
  * @property Customform_model customform_model
  * @property Inventory_model inventory_model
  * @property Dbitemdetails_model dbitemdetails_model
+ * @property Sritems_model sritems_model
  */
 
 class MY_Model extends CI_Model
