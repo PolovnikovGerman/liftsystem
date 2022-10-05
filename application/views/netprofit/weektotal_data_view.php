cef7f1a253288cd75c33c583cbc4b1e2fb571586
German <polovnikov.g@gmail.com>
2022-10-05 22:29:26 +0300

    Change netprofit update page

--- application/views/netprofit/weektotal_data_view.php
+++ application/views/netprofit/weektotal_data_view.php
@@ -8,8 +8,9 @@
 <div class="cell_payroll"><?= $out_payroll ?></div>
 <div class="cell_advertising"><?= $out_advertising ?></div>
 <div class="cell_projects"><?= $out_projects ?></div>
+<div class="cell_w9work"><?=$out_w9work?></div>
 <div class="cell_purchases"><?= $out_purchases ?></div>
-<div class="cell_inbut2">&nbsp;</div>
+<!-- <div class="cell_inbut2">&nbsp;</div> -->
 <div class="cell_total_costs2"><?= $out_totalcost?></div>
 <div class="cell_net_profit2"><?= $out_netprofit ?></div>
 <div class="cell_for_saved"><?= $out_saved ?></div>
