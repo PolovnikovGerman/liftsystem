9ba8a9c809e52c24f8d4887f0e64b3148a8c4f0d
German <polovnikov.g@gmail.com>
2022-10-08 00:58:23 +0300

    Fix isshue with hide Questions

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01978';
-$config['css_version']='2.01978';
+$config['js_version']='2.01979';
+$config['css_version']='2.01979';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
