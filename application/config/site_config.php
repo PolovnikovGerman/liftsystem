11936947a5773c6cd3c6ee4040ab784df85e3f6e
German <polovnikov.g@gmail.com>
2022-10-08 17:33:27 +0300

    Change netprofit update page

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version']='2.01982';
-$config['css_version']='2.01982';
+$config['js_version']='2.01983';
+$config['css_version']='2.01983';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
