1269055f1b224cccb71ea6faee7dd7e15246ddc9
German <polovnikov.g@gmail.com>
2022-11-10 11:22:04 +0200

    Lift - Net profit

---
+++ application/views/netprofitnew/new_category_view.php
@@ -0,0 +1,9 @@
+<div class="order_itemedit_area">
+    <div class="order_itemedit_title">Category <?=$category?></div>
+    <div class="order_itemedit">    
+        <input type="text" id="newcategoryvalue" value=""/>
+    </div>
+    <div class="newcategoryname_save">
+        <img src="/img/saveticket.png" alt="Save"/>
+    </div>    
+</div>
