01eff53195c3cb25ba12bc1d7db13d455257a9e9
German <polovnikov.g@gmail.com>
2022-11-16 00:15:56 +0200

    DB items (BT)

--- application/views/database_center/brand_database_view.php
+++ application/views/database_center/brand_database_view.php
@@ -61,3 +61,27 @@
         </div>
     </div>
 </div>
+<div class="modal fade" id="itemDetailsModal" tabindex="-1" role="dialog" aria-labelledby="itemDetailsModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><img src="/img/vendors/close_popup.png"></span></button>
+                <h4 class="modal-title" id="itemDetailsModalLabel">New message</h4>
+            </div>
+            <div class="modal-body" style="float: left;"></div>
+            <div class="modal-footer"></div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="itemImagesModal" tabindex="-1" role="dialog" aria-labelledby="itemImagesModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                <h4 class="modal-title" id="itemImagesModalLabel">New message</h4>
+            </div>
+            <div class="modal-body" style="float: left;"></div>
+            <div class="modal-footer"></div>
+        </div>
+    </div>
+</div>
