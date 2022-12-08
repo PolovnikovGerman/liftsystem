002b66998cf46719e0a1d0da218b0abbc1ec5f5a
German <polovnikov.g@gmail.com>
2022-12-08 19:32:02 +0200

    Inventory report totals

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02019';
-$config['css_version']= '2.02019';
+$config['js_version'] = '2.02020';
+$config['css_version']= '2.02020';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
