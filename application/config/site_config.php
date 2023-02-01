512d699f976eeed171ab45a403297eecc2995364
German <polovnikov.g@gmail.com>
2023-02-01 21:28:42 +0200

    Lift - Shipping Rates

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02030';
-$config['css_version']= '2.02030';
+$config['js_version'] = '2.02031';
+$config['css_version']= '2.02031';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
