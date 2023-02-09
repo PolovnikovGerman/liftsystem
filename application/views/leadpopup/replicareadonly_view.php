4777cb9f715bfaf5ac10bef4d6b33f175190ba88
German <polovnikov.g@gmail.com>
2023-02-09 17:24:13 +0200

    Lift - Quotes - Cosmetic fix

--- application/views/leadpopup/replicareadonly_view.php
+++ application/views/leadpopup/replicareadonly_view.php
@@ -1,10 +1,8 @@
 <div class="lead_popup_replicatiltle">Reps:</div>
-<div class="lead_popup_replicausrbgn">&nbsp;</div>
+<div class="lead_popup_replicausrarea">
 <?php $usrid=1;?>
 <?php foreach ($repl as $row) {?>
-    <div class="lead_popup_replicausrarea">
         <div class="lead_popup_replicausrname"><?=$row['user_leadname']?></div>
-    </div>
     <?php $usrid++?>
 <?php } ?>
-<div class="lead_popup_replicausrend">&nbsp;</div>
+</div>
--- application/views/leadpopup/replicaselect_view.php
+++ application/views/leadpopup/replicaselect_view.php
@@ -1,14 +1,14 @@
 <div class="lead_popup_replicatiltle">Reps:</div>
-<div class="lead_popup_replicausrbgn">&nbsp;</div>
+<!--<div class="lead_popup_replicausrbgn">&nbsp;</div>-->
+<div class="lead_popup_replicausrarea">
 <?php foreach ($repl as $row) {?>
-    <div class="lead_popup_replicausrarea">
-        <div class="lead_popup_replicausrchk" data-user="<?=($row['user_id'])?>" data-replic="<?=$row['user_leadname']?>">
-            <img src="/img/leads/delete-artloc.png"/>
-        </div>
-        <div class="lead_popup_replicausrname"><?=$row['user_leadname']?></div>
+    <div class="lead_popup_replicausrchk" data-user="<?=($row['user_id'])?>" data-replic="<?=$row['user_leadname']?>">
+        <img src="/img/leads/delete-artloc.png"/>
     </div>
+    <div class="lead_popup_replicausrname"><?=$row['user_leadname']?></div>
 <?php } ?>
-<div class="lead_popup_replicausrend">&nbsp;</div>
+</div>
+<!--<div class="lead_popup_replicausrend">&nbsp;</div>-->
 <div class="lead_popup_addreplica">
     <img alt="Add" src="/img/leads/addnew_btn.png">
 </div>
