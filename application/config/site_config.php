598ae7c573ee0b8e8a4db364fc56034b38456d83
German <polovnikov.g@gmail.com>
2023-02-24 00:07:38 +0200

    Paypal - new config

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02051';
-$config['css_version']= '2.02051';
+$config['js_version'] = '2.02052';
+$config['css_version']= '2.02052';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
