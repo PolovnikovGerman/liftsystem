82117c4dad9b4381a34519971910d3bc628ed15c
German <polovnikov.g@gmail.com>
2023-03-10 13:02:59 +0200

    Change COG details POPUP

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02069';
-$config['css_version']= '2.02069';
+$config['js_version'] = '2.02070';
+$config['css_version']= '2.02070';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
