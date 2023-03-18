88f67574020394bde5bd3421245cb561e1fab74a
German <polovnikov.g@gmail.com>
2023-03-18 02:11:07 +0200

    Change Profit (Orders) view

--- application/views/orderprofit/total_profitall_view.php
+++ application/views/orderprofit/total_profitall_view.php
@@ -15,20 +15,20 @@
         <div class="tax profitotaltooltip" data-viewsrc="<?=$tax_tooltip?>"><?= $data['show_tax'] ?></div>
         <div class="cog profitotaltooltip" data-viewsrc="<?=$cog_tooltip?>"><?= $data['show_cog'] ?></div>
         <div class="profitval  profitotaltooltip <?= $data['profit_class'] ?>" data-viewsrc="<?=$profit_tooltip?>"><?= $data['show_profit'] ?></div>
-        <div class="qty profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"
-             data-balloon="{ajax} <?=$qty_tooltip?>"><?= $data['qty'] ?></div>
-        <div class="revenue profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"
-             data-balloon="{ajax} <?=$revenue_tooltip?>"><?= $data['show_revenue'] ?></div>
-        <div class="balance profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"
-             data-balloon="{ajax} <?=$balance_tooltip?>" ><?= $data['show_balance'] ?></div>
-        <div class="shipping profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"
-             data-balloon="{ajax} <?=$shipping_tooltip?>" ><?= $data['show_shipping'] ?></div>
-        <div class="tax profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"
-             data-balloon="{ajax} <?=$tax_tooltip?>" ><?= $data['show_tax'] ?></div>
-        <div class="cog profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"
-             data-balloon="{ajax} <?=$cog_tooltip?>" ><?= $data['show_cog'] ?></div>
-        <div class="profitval  profitotaltooltip <?= $data['profit_class'] ?>" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"
-             data-balloon="{ajax} <?=$profit_tooltip?>" ><?= $data['show_profit'] ?></div>
+<!--        <div class="qty profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"-->
+<!--             data-balloon="{ajax} --><?php //=$qty_tooltip?><!--">--><?php //= $data['qty'] ?><!--</div>-->
+<!--        <div class="revenue profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"-->
+<!--             data-balloon="{ajax} --><?php //=$revenue_tooltip?><!--">--><?php //= $data['show_revenue'] ?><!--</div>-->
+<!--        <div class="balance profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"-->
+<!--             data-balloon="{ajax} --><?php //=$balance_tooltip?><!--" >--><?php //= $data['show_balance'] ?><!--</div>-->
+<!--        <div class="shipping profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"-->
+<!--             data-balloon="{ajax} --><?php //=$shipping_tooltip?><!--" >--><?php //= $data['show_shipping'] ?><!--</div>-->
+<!--        <div class="tax profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"-->
+<!--             data-balloon="{ajax} --><?php //=$tax_tooltip?><!--" >--><?php //= $data['show_tax'] ?><!--</div>-->
+<!--        <div class="cog profitotaltooltip" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"-->
+<!--             data-balloon="{ajax} --><?php //=$cog_tooltip?><!--" >--><?php //= $data['show_cog'] ?><!--</div>-->
+<!--        <div class="profitval  profitotaltooltip --><?php //= $data['profit_class'] ?><!--" data-bgcolor="#fff" data-event="hover" data-bordercolor="#000" data-textcolor="#000"-->
+<!--             data-balloon="{ajax} --><?php //=$profit_tooltip?><!--" >--><?php //= $data['show_profit'] ?><!--</div>-->
         <div class="profitperc <?= $data['profit_class'] ?>"><?= $data['profit_perc'] ?></div>
     </div>
 </div>
