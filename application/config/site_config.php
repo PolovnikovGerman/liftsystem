216aaf6ea1f3d7de513787fcb7e7924d545072f0
German <polovnikov.g@gmail.com>
2023-04-13 13:19:52 +0300

    Database page - Master Inventory

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02083';
-$config['css_version']= '2.02083';
+$config['js_version'] = '2.02084';
+$config['css_version']= '2.02084';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
