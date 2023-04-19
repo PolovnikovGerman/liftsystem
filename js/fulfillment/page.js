d16442758ed977e023dd7b79c24670a673d94f48
German <polovnikov.g@gmail.com>
2023-04-19 21:38:41 +0300

    Pages title

--- js/fulfillment/page.js
+++ js/fulfillment/page.js
@@ -21,30 +21,37 @@ function init_page(objid) {
         case 'vendorsview':
             $("#vendorsview").show();
             init_vendorpage();
+            document.title = 'Lift: Vendors';
             break;
         case 'fullfilstatusview':
             $("#fullfilstatusview").show();
             init_statuspage();
+            document.title = 'Lift: Status';
             break;
         case 'pototalsview':
             $("#pototalsview").show();
             init_purchase_orders();
+            document.title = 'Lift: PO Totals';
             break;
         case 'printshopinventview':
             $("#printshopinventview").show();
             init_inventory_content();
+            document.title = 'Lift: Inventory';
             break;
         case 'invneedlistview':
             $("#invneedlistview").show();
             init_needinvlist_content('fulfillment');
+            document.title = 'Lift: Inventory Need List';
             break;
         case 'salesrepinventview':
             $("#salesrepinventview").show();
             init_invsalesreport_content();
+            document.title = 'Lift: Sales Rep Inventory';
             break;
         case 'printshopreportview':
             $("#printshopreportview").show();
             init_orderreport_content();
+            document.title = 'Lift: Print Shop Report';
             break;
     }
 
