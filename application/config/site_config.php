e19dcf29fda92d4a1cdccbcad12269c0e2b2d919
German <polovnikov.g@gmail.com>
2023-05-02 23:10:13 +0300

    Master Inventory fix

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02092';
-$config['css_version']= '2.02092';
+$config['js_version'] = '2.02093';
+$config['css_version']= '2.02093';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
