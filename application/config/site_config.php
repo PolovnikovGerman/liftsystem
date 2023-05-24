8d22a199cb89ccf4045d17e1bbb20cc7c4b35328
German <polovnikov.g@gmail.com>
2023-05-24 20:41:29 +0300

    Orders list change

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02111';
-$config['css_version']= '2.02111';
+$config['js_version'] = '2.02112';
+$config['css_version']= '2.02112';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
