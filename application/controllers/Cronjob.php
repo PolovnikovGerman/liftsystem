0e6433800d525d9812dd6c24f2d97784036da04e
German <polovnikov.g@gmail.com>
2023-06-09 01:10:44 +0300

    Cronjob, PO Orders message

--- application/controllers/Cronjob.php
+++ application/controllers/Cronjob.php
@@ -495,10 +495,8 @@ Class Cronjob extends CI_Controller
         $config['wordwrap'] = TRUE;
         $this->email->initialize($config);
         $email_from=$this->config->item('email_notification_sender');
-        $email_to = 'to_german@yahoo.com';
-        $email_cc = $this->config->item('developer_email');
-        // $email_to=$this->config->item('sean_email');
-        // $email_cc=array($this->config->item('sage_email'),$this->config->item('developer_email'));
+        $email_to=$this->config->item('sean_email');
+        $email_cc=array($this->config->item('sage_email'));
         $this->email->from($email_from);
         $this->email->to($email_to);
         $this->email->cc($email_cc);
