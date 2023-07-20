43c7367dd01351930f95841293b982c262098abc
German <polovnikov.g@gmail.com>
2023-07-20 10:38:16 +0300

    Clay / Preview Export

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -8,8 +8,11 @@ class Export extends CI_Controller
     }
 
     public function claypreview_export() {
-        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+        // if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
             $postdata = $this->input->post();
+            foreach ($postdata as $key => $val) {
+                log_message('ERROR','POST '.$key.' - '.$val);
+            }
             $this->db->set('order_number', $postdata['order_number']);
             $this->db->set('doc_type', $postdata['doc_type']);
             $this->db->set('doc_link', $postdata['doc_link']);
@@ -22,14 +25,14 @@ class Export extends CI_Controller
             http_response_code('200');
             echo(json_encode($aResponse));
             exit;
-        } else {
-            $aResponse = array(
-                'data' => [],
-                'errors' => 'Incorrect request'
-            );
-            http_response_code('400');
-            echo(json_encode($aResponse));
-            exit;
-        }
+//        } else {
+//            $aResponse = array(
+//                'data' => [],
+//                'errors' => 'Incorrect request'
+//            );
+//            http_response_code('400');
+//            echo(json_encode($aResponse));
+//            exit;
+//        }
     }
 }
