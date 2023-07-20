57df7f8e421a3e911c668245441eac7ec017231b
German <polovnikov.g@gmail.com>
2023-07-20 18:50:17 +0300

    Clay / Preview Export

--- application/models/Artlead_model.php
+++ application/models/Artlead_model.php
@@ -1673,6 +1673,7 @@ Class Artlead_model extends MY_Model
     }
 
     private function _parse_claydoc($export, $artwork_id) {
+        ini_set("allow_url_fopen", true);
         $fullpath=$this->config->item('clay_models');
         $shrtpath=$this->config->item('clay_models_relative');
         $username = "stressballs";
@@ -1716,6 +1717,7 @@ Class Artlead_model extends MY_Model
     }
 
     private function _parse_previewpic($export, $artwork_id) {
+        ini_set("allow_url_fopen", true);
         $fullpath=$this->config->item('preview_pics');
         $shrtpath=$this->config->item('preview_pics_relative');
         $username = "stressballs";
