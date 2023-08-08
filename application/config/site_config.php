a21ad9d5805d123ea8cf230a1b5d442021c1516a
German <polovnikov.g@gmail.com>
2023-08-08 19:52:43 +0300

    DB items (SB) - fix notes

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02124';
-$config['css_version']= '2.02124';
+$config['js_version'] = '2.02125';
+$config['css_version']= '2.02125';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
