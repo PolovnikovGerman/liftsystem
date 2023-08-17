307bbb0906dee6b15f165cc78f7d1eec8d3b4287
German <polovnikov.g@gmail.com>
2023-08-17 20:36:49 +0300

    DB items (SB) - Item Notes B02

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02134';
-$config['css_version']= '2.02134';
+$config['js_version'] = '2.02135';
+$config['css_version']= '2.02135';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
