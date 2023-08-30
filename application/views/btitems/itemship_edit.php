1a2afb93d4a2ec81a5fd26a8c2ddb63f8b1bfc79
German <polovnikov.g@gmail.com>
2023-08-30 21:13:16 +0300

    DB items (SB) - Item Notes C02 (missing info BG)

--- application/views/btitems/itemship_edit.php
+++ application/views/btitems/itemship_edit.php
@@ -9,49 +9,47 @@
             <div class="itemparamlabel weight-measure">lbs</div>
             <div class="itemparamlabel specialeach">Extra $ Each:</div>
             <div class="itemparamvalue specialeach editmode">
-                <input type="text" class="iteminfoinput chargepereach" data-item="charge_pereach" value="<?=$item['charge_pereach']?>"/>
+                <input type="text" class="itemkeyinfoinput chargepereach" data-item="charge_pereach" value="<?=$item['charge_pereach']?>"/>
             </div>
         </div>
-        <div class="itemboxarea">
-            <div class="content-row">
-                <div class="itemparamlabel boxnametitle">Box</div>
-                <div class="itemparamlabel boxqtytitle">Qty</div>
+        <div class="content-row">
+            <div class="itemboxarea">
+                <div class="content-row">
+                    <div class="itemparamlabel boxnametitle">Box</div>
+                    <div class="itemparamlabel boxqtytitle">Qty</div>
+                </div>
             </div>
-            <div class="itemboxdataarea">
-                <?php $numpp=1;?>
-                <?php foreach ($boxes as $box) {?>
-                    <div class="content-row">
-                        <div class="itemparamlabel boxname"><?=chr(64 + $numpp)?></div>
-                        <div class="itemparamvalue boxqty editmode">
-                            <input type="text" class="itemshipbox box_qty" data-shipbox="<?=$box['item_shipping_id']?>" data-item="box_qty" value="<?=$box['box_qty']?>"/>
-                        </div>
+            <div class="itemdimesionsarea">
+                <div class="content-row">
+                    <div class="itemparamlabel dimensiontitle">Dimensions (Inches)</div>
+                </div>
+            </div>
+        </div>
+        <?php $numpp=1;?>
+        <?php foreach ($boxes as $box) {?>
+            <div class="content-row">
+                <div class="itemboxdataarea">
+                    <div class="itemparamlabel boxname"><?=chr(64 + $numpp)?></div>
+                    <div class="itemparamvalue boxqty editmode">
+                        <input type="text" class="itemshipbox box_qty" data-shipbox="<?=$box['item_shipping_id']?>" data-item="box_qty" value="<?=$box['box_qty']?>"/>
                     </div>
                     <?php $numpp++;?>
-                <?php } ?>
-            </div>
-        </div>
-        <div class="itemdimesionsarea">
-            <div class="content-row">
-                <div class="itemparamlabel dimensiontitle">Dimensions (Inches)</div>
-            </div>
-            <div class="itemdimensionsdataarea">
-                <?php foreach ($boxes as $box) {?>
-                    <div class="content-row">
-                        <div class="itemparamvalue boxwidth editmode">
-                            <input type="text" class="itemshipbox box_width" data-shipbox="<?=$box['item_shipping_id']?>" data-item="box_width" value="<?=$box['box_width']?>"/>
-                        </div>
-                        <div class="itemparamlabel boxwidth">W</div>
-                        <div class="itemparamvalue boxdepth editmode">
-                            <input type="text" class="itemshipbox box_length" data-shipbox="<?=$box['item_shipping_id']?>" data-item="box_length" value="<?=$box['box_length']?>"/>
-                        </div>
-                        <div class="itemparamlabel boxdepth">L</div>
-                        <div class="itemparamvalue boxheight editmode">
-                            <input type="text" class="itemshipbox box_height" data-shipbox="<?=$box['item_shipping_id']?>" data-item="box_height" value="<?=$box['box_height']?>"/>
-                        </div>
-                        <div class="itemparamlabel boxheight">H</div>
+                </div>
+                <div class="itemdimensionsdataarea">
+                    <div class="itemparamvalue boxwidth editmode">
+                        <input type="text" class="itemshipbox box_width" data-shipbox="<?=$box['item_shipping_id']?>" data-item="box_width" value="<?=$box['box_width']?>"/>
                     </div>
-                <?php } ?>
+                    <div class="itemparamlabel boxwidth">W</div>
+                    <div class="itemparamvalue boxdepth editmode">
+                        <input type="text" class="itemshipbox box_length" data-shipbox="<?=$box['item_shipping_id']?>" data-item="box_length" value="<?=$box['box_length']?>"/>
+                    </div>
+                    <div class="itemparamlabel boxdepth">L</div>
+                    <div class="itemparamvalue boxheight editmode">
+                        <input type="text" class="itemshipbox box_height" data-shipbox="<?=$box['item_shipping_id']?>" data-item="box_height" value="<?=$box['box_height']?>"/>
+                    </div>
+                    <div class="itemparamlabel boxheight">H</div>
+                </div>
             </div>
-        </div>
+        <?php } ?>
     </div>
 </div>
