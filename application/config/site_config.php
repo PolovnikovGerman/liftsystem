0ffd49d5220233062ded685dac0a29415bab15cc
German <polovnikov.g@gmail.com>
2023-09-01 18:57:26 +0300

    Lift - left submenu

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02153';
-$config['css_version']= '2.02153';
+$config['js_version'] = '2.02154';
+$config['css_version']= '2.02154';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
