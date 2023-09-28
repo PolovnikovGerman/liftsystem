4cf216ae4ae5d52a1792a34ecc095b81c27dcb8f
German <polovnikov.g@gmail.com>
2023-09-28 22:48:53 +0300

    Sigma system

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02166';
-$config['css_version']= '2.02166';
+$config['js_version'] = '2.02167';
+$config['css_version']= '2.02167';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
