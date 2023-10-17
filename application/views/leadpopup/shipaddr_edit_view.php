942cf3a86f371b9ecddf458352537ba64602f221
German <polovnikov.g@gmail.com>
2023-10-17 22:11:26 +0300

    Google Autoplace Address (lead)

--- application/views/leadpopup/shipaddr_edit_view.php
+++ application/views/leadpopup/shipaddr_edit_view.php
@@ -1,2 +1,2 @@
 <input class="quoteaddressinpt quoteshipadrother" data-item="shipping_address1"
-       placeholder="Address Line 1" value="<?=$data['shipping_address1']?>" id="quoteshipaddress_line1"/>
+       placeholder="Address Line 1" value="<?=$data['shipping_address1']?>" id="quoteshipaddress_line1" autocomplete="new-password"/>
