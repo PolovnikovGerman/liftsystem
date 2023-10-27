5d9632de400266070cd905ed44b79ed74b9b7e3d
German <polovnikov.g@gmail.com>
2023-10-27 17:13:16 +0300

    Change adresses

--- application/views/leadpopup/shipaddr_edit_view.php
+++ application/views/leadpopup/shipaddr_edit_view.php
@@ -1,2 +1,2 @@
-<input class="quoteaddressinpt quoteshipadrother" data-item="shipping_address1"
-       placeholder="Address Line 1" value="<?=$data['shipping_address1']?>" id="quoteshipaddress_line1" autocomplete="new-password"/>
+<input type="text" class="quoteadrinpt quoteshipadrother" data-item="shipping_address1" <?=$mapuse==0 ? 'placeholder="Address Line 1"' : ''?>
+       value="<?=$data['shipping_address1']?>" id="shiplead_line1" autocomplete="new-password" aria-autocomplete="none"/>
