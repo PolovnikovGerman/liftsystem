b8c0e0dbd8da777c9fd1019c55cedb4e4db1479e
German <polovnikov.g@gmail.com>
2023-12-09 01:53:37 +0200

    Copy contacts emails

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02192';
-$config['css_version']= '2.02192';
+$config['js_version'] = '2.02195';
+$config['css_version']= '2.02195';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
