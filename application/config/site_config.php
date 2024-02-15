c6de0265cdd57719a5476e7a0a8de505a16f6230
German <polovnikov.g@gmail.com>
2024-02-15 10:47:23 +0200

    User Settings fo export

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02218';
-$config['css_version']= '2.02218';
+$config['js_version'] = '2.02219';
+$config['css_version']= '2.02219';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
