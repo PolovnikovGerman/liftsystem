795a576d930924df5ca4507623f1e02c095aab98
German <polovnikov.g@gmail.com>
2024-02-17 00:32:59 +0200

    Repeat setup fix

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02220';
-$config['css_version']= '2.02220';
+$config['js_version'] = '2.02221';
+$config['css_version']= '2.02221';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
