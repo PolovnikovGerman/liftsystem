cae723a89235620f00f2eea1fc05717cbccde915
German <polovnikov.g@gmail.com>
2024-03-08 00:54:12 +0200

    Change Add item function

--- application/views/leadorderdetails/items_data_edit.php
+++ application/views/leadorderdetails/items_data_edit.php
@@ -22,7 +22,7 @@
         <div class="items_content_each2 bord_l">
             <input type="text" class="orderitem_price input_text_right orderitem_input input_border_gray <?=$row['qtyinput_class']?>" title="<?=$row['qtyinput_title']?>" data-field="item_price" data-item="<?=$row['item_id']?>" data-orderitem="<?=$row['order_item_id']?>" value="<?=PriceOutput($row['item_price'])?>" />
         </div>
-        <div class="items_content_sub_total2 bord_l" data-item="<?=$row['item_id']?>" data-orderitem="<?=$row['order_item_id']?>"><?=$row['item_subtotal']?></div>
+        <div class="items_content_sub_total2 bord_l" data-item="<?=$row['item_id']?>" data-orderitem="<?=$row['order_item_id']?>"><?=empty($row['item_subtotal']) ? '' : MoneyOutput($row['item_subtotal'])?></div>
         <div class="items_content_trash2 bord_l">
             <?php if ($row['item_row']==1) { ?>
             <i class="fa fa-trash" data-orderitem="<?=$row['order_item_id']?>" data-item="<?=$row['item_description']?>"></i>
