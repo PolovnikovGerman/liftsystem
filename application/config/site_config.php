8874cbca09036fc6e769e366260222246258be78
German <polovnikov.g@gmail.com>
2024-03-08 21:29:59 +0200

    Change Add item function

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02238';
-$config['css_version']= '2.02238';
+$config['js_version'] = '2.02239';
+$config['css_version']= '2.02239';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
