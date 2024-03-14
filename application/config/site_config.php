3e4606b1942a9d6fdc8da7a88ee4ea166de63cd1
German <polovnikov.g@gmail.com>
2024-03-14 11:52:49 +0200

    Change Add item function - calc shipping after add item

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02240';
-$config['css_version']= '2.02240';
+$config['js_version'] = '2.02241';
+$config['css_version']= '2.02241';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
