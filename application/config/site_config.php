a70b5b086b5fd5ee4b7226d52a4a0f83b3ded456
German <polovnikov.g@gmail.com>
2024-03-15 11:18:43 +0200

    Orders cosmetic fix

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02243';
-$config['css_version']= '2.02243';
+$config['js_version'] = '2.02244';
+$config['css_version']= '2.02244';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
