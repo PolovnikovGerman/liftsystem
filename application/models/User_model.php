0cabe5445cebf5196a80d9267a22e43f585f2fa0
German <polovnikov.g@gmail.com>
2024-04-09 19:33:36 +0300

    Change base url, break user connection if password / status changed

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -14,18 +14,17 @@ Class User_model extends MY_Model
     public function current_user() {
         $this->load->helper('cookie');
         $out = ['result' =>$this->error_result,];
-        // $user=usersession('usr_data');
+        $user=usersession('usr_data');
         $user = [];
         $cookie=get_cookie('acctoken');
-        //if (empty($user)) {
+        if (empty($user)) {
             if ($cookie) {
                 $res=$this->get_user_accesstoken($cookie);
                 if ($res['result']==$this->success_result) {
                     $user=$res['user'];
                 }
             }
-        //}
-        // if (is_array($user)) {
+        }
         if (ifset($user,'id',0)>0) {
             /* Try to check Cooikie */
             $this->db->select('user_status');
@@ -35,7 +34,7 @@ Class User_model extends MY_Model
             if ($chkres['user_status']==1) {
                 $out['result'] = $this->success_result;
                 $out['data'] = $user;
-                // usersession('usr_data', $user);
+                usersession('usr_data', $user);
                 $server=$this->input->server('SERVER_NAME');
                 $cookienew = array(
                     'name'   => 'acctoken',
@@ -161,16 +160,16 @@ Class User_model extends MY_Model
                             $out['result'] = $this->success_result;
                             $out['msg'] = '';
                             $out['usrdat'] = $res;
-//                            $usr_data = array(
-//                                'id' => $res['user_id'],
-//                                'user_logged_in'=> $res['role_short'],
-//                                'user_email' => $res['user_email'],
-//                                'user_name'=>$res['user_name'],
-//                                'user_replica'=>(!empty($res['user_leadname']) ? $res['user_leadname'] : $res['first_name']),
-//                                'user_logo' => (empty($res['user_logo']) ? $this->config->item('empty_profile') : $res['user_logo']),
-//                                'user_order_export' => $res['user_order_export'],
-//                            );
-//                            usersession('usr_data', $usr_data);
+                            $usr_data = array(
+                                'id' => $res['user_id'],
+                                'user_logged_in'=> $res['role_short'],
+                                'user_email' => $res['user_email'],
+                                'user_name'=>$res['user_name'],
+                                'user_replica'=>(!empty($res['user_leadname']) ? $res['user_leadname'] : $res['first_name']),
+                                'user_logo' => (empty($res['user_logo']) ? $this->config->item('empty_profile') : $res['user_logo']),
+                                'user_order_export' => $res['user_order_export'],
+                            );
+                            usersession('usr_data', $usr_data);
                             // Create access token
                             $res['token']=uniq_link(30);
                             /* Save token to DB */
@@ -391,15 +390,15 @@ Class User_model extends MY_Model
         $this->db->where('u.user_id', $user_id);
         $res = $this->db->get()->row_array();
 
-//        $usr_data = array(
-//            'id' => $res['user_id'],
-//            'user_logged_in'=> $res['role_short'],
-//            'user_email' => $res['user_email'],
-//            'user_name'=>$res['user_name'],
-//            'user_replica'=>(!empty($res['user_leadname']) ? $res['user_leadname'] : $res['first_name']),
-//            'user_logo' => (empty($res['user_logo']) ? $this->config->item('empty_profile') : $res['user_logo']),
-//        );
-//        usersession('usr_data', $usr_data);
+        $usr_data = array(
+            'id' => $res['user_id'],
+            'user_logged_in'=> $res['role_short'],
+            'user_email' => $res['user_email'],
+            'user_name'=>$res['user_name'],
+            'user_replica'=>(!empty($res['user_leadname']) ? $res['user_leadname'] : $res['first_name']),
+            'user_logo' => (empty($res['user_logo']) ? $this->config->item('empty_profile') : $res['user_logo']),
+        );
+        usersession('usr_data', $usr_data);
         return TRUE;
 
     }
@@ -712,16 +711,16 @@ Class User_model extends MY_Model
         $this->db->join('roles r','r.role_id=u.role_id','left');
         $this->db->where('u.user_id', $user_id);
         $res = $this->db->get()->row_array();
-//        $usr_data = array(
-//            'id' => $res['user_id'],
-//            'user_logged_in'=> $res['role_short'],
-//            'user_email' => $res['user_email'],
-//            'user_name'=>$res['user_name'],
-//            'user_replica'=>(!empty($res['user_leadname']) ? $res['user_leadname'] : $res['first_name']),
-//            'user_logo' => (empty($res['user_logo']) ? $this->config->item('empty_profile') : $res['user_logo']),
-//            'user_order_export' => $res['user_order_export'],
-//        );
-//        usersession('usr_data', $usr_data);
+        $usr_data = array(
+            'id' => $res['user_id'],
+            'user_logged_in'=> $res['role_short'],
+            'user_email' => $res['user_email'],
+            'user_name'=>$res['user_name'],
+            'user_replica'=>(!empty($res['user_leadname']) ? $res['user_leadname'] : $res['first_name']),
+            'user_logo' => (empty($res['user_logo']) ? $this->config->item('empty_profile') : $res['user_logo']),
+            'user_order_export' => $res['user_order_export'],
+        );
+        usersession('usr_data', $usr_data);
         return TRUE;
     }
 
