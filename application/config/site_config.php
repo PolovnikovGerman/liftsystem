d8de8affb4b912bad255f787f58e540f3cec145a
German <polovnikov.g@gmail.com>
2024-04-19 23:24:53 +0300

    New header

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02271';
-$config['css_version']= '2.02271';
+$config['js_version'] = '2.02272';
+$config['css_version']= '2.02272';
 /* Default Profit percent */
 $config['default_profit']=40;
 $config['default_brand']=1;
