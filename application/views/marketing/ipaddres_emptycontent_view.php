e69f539f61206950cd7bab0796015f584a2715eb
German <polovnikov.g@gmail.com>
2024-05-11 00:59:14 +0300

    Marketing tab view (new design)

--- application/views/marketing/ipaddres_emptycontent_view.php
+++ application/views/marketing/ipaddres_emptycontent_view.php
@@ -1,9 +1,3 @@
 <div class="searchesipaddrsarea">
-    <div class="searchesipaddrsubhead">
-        <div class="rank">#</div>
-        <div class="keyword">IP Address</div>
-        <div class="result">Qty</div>
-        <div class="location">Location</div>
-    </div>
     <div class="searchesipaddrempty">No data about Searches for this period</div>
 </div>
