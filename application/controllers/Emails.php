62851fde43d318ce7d09360916bdc81a038db548
German <polovnikov.g@gmail.com>
2024-05-24 13:53:55 +0300

    Email client

--- application/controllers/Emails.php
+++ application/controllers/Emails.php
@@ -1,6 +1,37 @@
 <?php
+use SSilence\ImapClient\ImapClientException;
+use SSilence\ImapClient\ImapConnect;
+use SSilence\ImapClient\ImapClient as Imap;
 
-class Emails
+class Emails extends MY_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
+    public function index()
+    {
+
+    }
+
+    public function test()
+    {
+        phpinfo();die();
+        $mailbox = 'mail.yahoosmallbusiness.com';
+        $username = 'german.polovnikov@bluetrack.com'; // german.polovnikov@bluetrack.com
+        $password = 'Brilliance70!';
+        $encryption = Imap::ENCRYPT_SSL;
+
+        try{
+            $imap = new Imap($mailbox, $username, $password, $encryption);
+            // You can also check out example-connect.php for more connection options
+
+        }catch (ImapClientException $error){
+            echo $error->getMessage().PHP_EOL; // You know the rule, no errors in production ...
+            die(); // Oh no :( we failed
+        }
+        echo 'Imap Open successfully'.PHP_EOL;
+        die();
+    }
 }
