b787b9e21b1c23ed8f65312b9b56448200e51281
German <polovnikov.g@gmail.com>
2024-05-31 19:47:23 +0300

    Email client

--- application/database/migrations/20240531155142_users_mailparams.php
+++ application/database/migrations/20240531155142_users_mailparams.php
@@ -3,20 +3,40 @@
 class Migration_users_mailparams extends CI_Migration {
 
     public function up() {
-
         $this->dbforge->add_field(array(
-            'id' => array(
+            'postbox_id' => array(
                 'type' => 'INT',
                 'constraint' => 11,
                 'auto_increment' => TRUE
-            )
+            ),
+            'user_id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'null' => true
+            ),
+            'mailbox' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ),
+            'postbox_user' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ),
+            'postbox_passwd' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ),
         ));
-        $this->dbforge->add_key('id', TRUE);
-        $this->dbforge->create_table('users_mailparams');
+        $this->dbforge->add_key('postbox_id', TRUE);
+        $this->dbforge->add_field('CONSTRAINT FOREIGN KEY (user_id) REFERENCES users(user_id) on update cascade on delete set null');
+        $this->dbforge->create_table('user_postboxes');
     }
 
     public function down() {
-        $this->dbforge->drop_table('users_mailparams');
+        $this->dbforge->drop_table('user_postboxes');
     }
 
 }
