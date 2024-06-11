fa7b7b22c4f0c8eb69e0d59a4de293d8aaa41c30
German <polovnikov.g@gmail.com>
2024-06-11 23:51:57 +0300

    Inventory seq fix

--- application/models/Inventory_model.php
+++ application/models/Inventory_model.php
@@ -144,7 +144,7 @@ class Inventory_model extends MY_Model
                         'item_id' => $item['inventory_item_id'],
                         'item_flag' =>0,
                         'status' => ($color['notreorder']==1 ? $this->donotreorder : ''), // ($color['color_status']==1 ? 'Active' : 'Inactive')),
-                        'item_seq' => $color['color_order'], // $color_seq,
+                        'item_seq' => $color_seq, // $color['color_order'], //
                         'item_code' => '',
                         'description' => $color['color'],
                         'max' => $max,
