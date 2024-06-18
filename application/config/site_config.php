70cbe9737578c41063c1121cd18573c74888f7f7
German <polovnikov.g@gmail.com>
2024-06-19 01:08:34 +0300

    Change Inventory view

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02309';
-$config['css_version']= '2.02309';
+$config['js_version'] = '2.02310';
+$config['css_version']= '2.02310';
 /* SET EMPTY SERVER NAME */
 if (!isset($_SERVER['SERVER_NAME'])) {
     $_SERVER['SERVER_NAME'] = getenv('server_name');
