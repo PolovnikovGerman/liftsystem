c86c8d9bdddc0888f578e5ebd0628dabc7ca0b56
german_zp <to_german@yahoo.com>
2024-06-26 02:11:34 +0300

    Accounts receivable rebuild

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02314';
-$config['css_version']= '2.02314';
+$config['js_version'] = '2.02315';
+$config['css_version']= '2.02315';
 /* SET EMPTY SERVER NAME */
 if (!isset($_SERVER['SERVER_NAME'])) {
     $_SERVER['SERVER_NAME'] = getenv('server_name');
