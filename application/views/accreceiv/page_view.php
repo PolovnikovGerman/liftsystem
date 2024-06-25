dd4f07bd72ee63558b9070ead50408af58877b82
german_zp <to_german@yahoo.com>
2024-06-25 20:45:09 +0300

    Accounts receivable rebuild

--- application/views/accreceiv/page_view.php
+++ application/views/accreceiv/page_view.php
@@ -1,7 +1,7 @@
 <div class="accreceiv-content">
     <input type="hidden" id="accreceivebrand" value="<?=$brand?>">
     <input type="hidden" id="accreciveownsort" value="batch_due"/>
-    <input type="hidden" id="accreciveowndir" value="desc"/>
+    <input type="hidden" id="accreciveowndir" value="asc"/>
     <input type="hidden" id="accreceiverefundsort" value="order_date"/>
     <input type="hidden" id="accreceiverefunddir" value="desc"/>
     <div class="accreceive-title">
