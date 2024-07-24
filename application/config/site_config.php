8f7bf41366b240f71741342ccfa27d1046b5bff4
German <polovnikov.g@gmail.com>
2024-07-24 17:41:11 +0300

    Tracking codes - change relation

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02324';
-$config['css_version']= '2.02324';
+$config['js_version'] = '2.02325';
+$config['css_version']= '2.02325';
 /* SET EMPTY SERVER NAME */
 if (!isset($_SERVER['SERVER_NAME'])) {
     $_SERVER['SERVER_NAME'] = getenv('server_name');
