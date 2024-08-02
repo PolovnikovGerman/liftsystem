9249def5c60f58550b29ef64cc99418e57999afc
German <polovnikov.g@gmail.com>
2024-08-02 19:05:23 +0300

    Change Lift Header

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02338';
-$config['css_version']= '2.02338';
+$config['js_version'] = '2.02339';
+$config['css_version']= '2.02339';
 /* SET EMPTY SERVER NAME */
 if (!isset($_SERVER['SERVER_NAME'])) {
     $_SERVER['SERVER_NAME'] = getenv('server_name');
