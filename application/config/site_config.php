537e8c9a7983d541eb79bb3653e7db23f82c8a91
German <polovnikov.g@gmail.com>
2024-08-12 20:26:00 +0300

    Print Scheduler

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02346';
-$config['css_version']= '2.02346';
+$config['js_version'] = '2.02347';
+$config['css_version']= '2.02347';
 /* SET EMPTY SERVER NAME */
 if (!isset($_SERVER['SERVER_NAME'])) {
     $_SERVER['SERVER_NAME'] = getenv('server_name');
