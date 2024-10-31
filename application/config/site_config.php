5c3ead6a0be96ae7eafd8b1411edfe5e71b7ab4b
German <polovnikov.g@gmail.com>
2024-10-31 09:16:08 +0200

    PO Overview & History

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02389';
-$config['css_version']= '2.02389';
+$config['js_version'] = '2.02390';
+$config['css_version']= '2.02390';
 /* SET EMPTY SERVER NAME */
 if (!isset($_SERVER['SERVER_NAME'])) {
     $_SERVER['SERVER_NAME'] = getenv('server_name');
