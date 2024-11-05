0245ba12116bf5de7f5ec5c9075b74d6dd79770f
German <polovnikov.g@gmail.com>
2024-11-05 22:10:47 +0200

    PO Report fix

--- application/database/migrations/20241105213451_alt_orders.php
+++ application/database/migrations/20241105213451_alt_orders.php
@@ -3,19 +3,14 @@
 class Migration_alt_orders extends CI_Migration {
 
     public function up() {
-        $this->dbforge->add_field(array(
-            'id' => array(
-                'type' => 'INT',
-                'constraint' => 11,
-                'auto_increment' => TRUE
-            )
-        ));
-        $this->dbforge->add_key('id', TRUE);
-        $this->dbforge->create_table('alt_orders');
+        $fields = array(
+            'debtstatus_date' => array('type' => 'INT', 'constraint' => 14, 'null' => true, 'comment' => 'Debt Status Date'),
+        );
+        $this->dbforge->add_column('ts_orders', $fields);
     }
 
     public function down() {
-        $this->dbforge->drop_table('alt_orders');
+        $this->dbforge->drop_column('ts_orders', 'debtstatus_date');
     }
 
 }
