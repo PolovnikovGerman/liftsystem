a45564f3f1b0f8f51dee6c6ce0865e968503fe0a
German <polovnikov.g@gmail.com>
2024-12-12 14:29:21 +0200

    Header (Leads / visitors)

--- application/views/page/dashboard_total_view.php
+++ application/views/page/dashboard_total_view.php
@@ -1,14 +1,41 @@
-<div class="period_name"><?=$data['label']?></div>
-<div class="totalsalesprev <?=$data['prev_navig']==1 ? 'active' : ''?>" data-week="<?=$data['prev_week']?>">
-    <i class="fa fa-caret-left"></i>
+<div class="period_lead_info">
+    <div class="datarow">
+        <div class="leadsection visitors">
+            <div class="period_name">Visitors:</div>
+        </div>
+        <div class="leadsection leads">
+            <div class="period_name">Leads:</div>
+        </div>
+    </div>
+    <div class="datarow" style="cursor:pointer" data-event="click" data-css="weekbrandtotals" data-bgcolor="#000000"
+         data-bordercolor="#adadad" data-textcolor="#FFFFFF"
+         data-position="down"
+         data-balloon="{ajax} /welcome/weektotalvisitors/<?= $data['currweek'] ?>">
+        <div class="leadsection visitors">
+            <div class="param_value"><?= $data['visitors'] ?></div>
+        </div>
+        <div class="leadsection leads">
+            <div class="param_value"><?= $data['leads'] ?></div>
+        </div>
+    </div>
 </div>
-<div class="period_results">
-    <div class="param_value" id="totalsales" data-event="click" data-css="weekbrandtotals" data-bgcolor="#000000" data-bordercolor="#adadad" data-textcolor="#FFFFFF"
-         data-position="down" data-balloon="{ajax} /welcome/weektotalorders/<?=$data['currweek']?>"><?=$data['sales']?></div>
-    <div class="param_value_label"> Orders <span>@</span> </div>
-    <div class="param_value money" id="totalrevenue" data-event="click" data-css="weekbrandtotals" data-bgcolor="#000000" data-bordercolor="#adadad" data-textcolor="#FFFFFF"
-         data-position="down" data-balloon="{ajax} /welcome/weektotals/<?=$data['currweek']?>"><?=MoneyOutput($data['revenue'],0)?></div>
-</div>
-<div class="totalsalesnext <?=$data['next_navig']==1 ? 'active' : ''?>" data-week="<?=$data['next_week']?>">
-    <i class="fa fa-caret-right"></i>
+<div class="period_analitic_info">
+    <div class="period_name"><?= $data['label'] ?></div>
+    <div class="totalsalesprev <?= $data['prev_navig'] == 1 ? 'active' : '' ?>" data-week="<?= $data['prev_week'] ?>">
+        <i class="fa fa-caret-left"></i>
+    </div>
+    <div class="period_results">
+        <div class="param_value" id="totalsales" data-event="click" data-css="weekbrandtotals" data-bgcolor="#000000"
+             data-bordercolor="#adadad" data-textcolor="#FFFFFF"
+             data-position="down"
+             data-balloon="{ajax} /welcome/weektotalorders/<?= $data['currweek'] ?>"><?= $data['sales'] ?></div>
+        <div class="param_value_label"> Orders <span>@</span></div>
+        <div class="param_value money" id="totalrevenue" data-event="click" data-css="weekbrandtotals"
+             data-bgcolor="#000000" data-bordercolor="#adadad" data-textcolor="#FFFFFF"
+             data-position="down"
+             data-balloon="{ajax} /welcome/weektotals/<?= $data['currweek'] ?>"><?= MoneyOutput($data['revenue'], 0) ?></div>
+    </div>
+    <div class="totalsalesnext <?= $data['next_navig'] == 1 ? 'active' : '' ?>" data-week="<?= $data['next_week'] ?>">
+        <i class="fa fa-caret-right"></i>
+    </div>
 </div>
