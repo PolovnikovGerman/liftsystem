d43b684e801d4e4c7a2d1b3f8604f8b012372643
German Polovnikov <polovnikov.g@gmail.com>
2025-03-22 20:31:41 +0200

    SMTP send messages (Quota)

--- application/config/notifications.php
+++ application/config/notifications.php
@@ -1,1 +1,7 @@
 <?php
+$config['sb_smtp_host'] = getenv('SB_SMTP_HOST');
+$config['sb_smtp_port'] = getenv('SB_SMTP_PORT');
+$config['sb_smtp_crypto'] = 'ssl';
+$config['sb_quote_smtp'] = getenv('SB_QUOTE_SMTP');
+$config['sb_quote_user'] = getenv('SB_QUOTE_USER');
+$config['sb_quote_pass'] = getenv('SB_QUOTE_PASS');
