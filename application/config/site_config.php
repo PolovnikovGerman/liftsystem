1f4e9249667ed7ae1bfc93c48696f48f2aaeba54
German Polovnikov <polovnikov.g@gmail.com>
2025-03-29 00:47:41 +0200

    Print Schedule - relation - amounts - order items

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02430';
-$config['css_version']= '2.02430';
+$config['js_version'] = '2.02432';
+$config['css_version']= '2.02432';
 /* SET EMPTY SERVER NAME */
 if (!isset($_SERVER['SERVER_NAME'])) {
     $_SERVER['SERVER_NAME'] = getenv('server_name');
