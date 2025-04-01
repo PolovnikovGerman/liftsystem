509c171f54a359905b966293a81d8ef532fe551a
German Polovnikov <polovnikov.g@gmail.com>
2025-04-01 22:02:25 +0300

    Custom Quotes - weeks report

--- application/views/customsbforms/totals_data_view.php
+++ application/views/customsbforms/totals_data_view.php
@@ -2,14 +2,14 @@
 <?php foreach ($totals as $total): ?>
     <div class="datarow <?=$numpp%2==0 ? 'whitedatarow' : 'greydatarow'?>">
         <div class="total_weeknum"><?=$total['week']?></div>
-        <div class="total_day"><?=$total['mon']?></div>
-        <div class="total_day"><?=$total['tue']?></div>
-        <div class="total_day"><?=$total['wed']?></div>
-        <div class="total_day"><?=$total['thu']?></div>
-        <div class="total_day"><?=$total['fri']?></div>
-        <div class="total_day"><?=$total['sat']?></div>
-        <div class="total_day"><?=$total['sun']?></div>
-        <div class="total_totals"><?=$total['total']?></div>
+        <div class="total_day"><?=empty($total['mon']) ? '&nbsp;' : $total['mon']?></div>
+        <div class="total_day"><?=empty($total['tue']) ? '&nbsp;' : $total['tue']?></div>
+        <div class="total_day"><?=empty($total['wed']) ? '&nbsp;' : $total['wed']?></div>
+        <div class="total_day"><?=empty($total['thu']) ? '&nbsp;' : $total['thu']?></div>
+        <div class="total_day"><?=empty($total['fri']) ? '&nbsp;' : $total['fri']?></div>
+        <div class="total_day"><?=empty($total['sat']) ? '&nbsp;' : $total['sat']?></div>
+        <div class="total_day"><?=empty($total['sun']) ? '&nbsp;' : $total['sun']?></div>
+        <div class="total_totals"><?=empty($total['total']) ? '&nbsp;' : $total['total']?></div>
     </div>
     <?php $numpp++; ?>
 <?php endforeach; ?>
