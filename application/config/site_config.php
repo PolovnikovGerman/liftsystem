2960405990e7517250357c3d01e948c3d1c5ba42
German Polovnikov <polovnikov.g@gmail.com>
2025-04-01 11:54:38 +0300

    Custom Quotes - weeks report

--- application/config/site_config.php
+++ application/config/site_config.php
@@ -1,6 +1,6 @@
 <?php
-$config['js_version'] = '2.02433';
-$config['css_version']= '2.02433';
+$config['js_version'] = '2.02434';
+$config['css_version']= '2.02434';
 /* SET EMPTY SERVER NAME */
 if (!isset($_SERVER['SERVER_NAME'])) {
     $_SERVER['SERVER_NAME'] = getenv('server_name');
