e4680e3492126539fef9166249563e8fcdfd19f0
German Polovnikov <polovnikov.g@gmail.com>
2025-05-11 15:38:01 +0300

    New menu and tabs

--- application/views/test/test_tabs.php
+++ application/views/test/test_tabs.php
@@ -55,13 +55,11 @@
                     </div>
                 </div>
                 <div class="period_review_info">
-                    <!-- <div class="reviewsection"> -->
-                        <div class="period_name">Reviews:</div>
-                        <div class="param_value">122</div>
-                    <!-- </div> -->
+                    <div class="period_name">Reviews:</div>
+                    <div class="param_value">122</div>
                 </div>
                 <div class="period_analitic_info">
-                    <div class="period_name">ALL BRANDS THIS WEEK:</div>
+                    <div class="period_name">APR/MAY 28-4, 2025:</div>
                     <div class="totalsalesprev active">
                         <i class="fa fa-caret-left"></i>
                     </div>
