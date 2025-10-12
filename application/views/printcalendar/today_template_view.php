0424c882174332930d5d6ec07616d539655fbacb
German Polovnikov <polovnikov.g@gmail.com>
2025-10-12 23:02:26 +0300

    Print Schedule New (cosmetic fix)

--- application/views/printcalendar/today_template_view.php
+++ application/views/printcalendar/today_template_view.php
@@ -1,1 +1,22 @@
-<?php
+<div class="regular-table">
+    <div class="regltabl-tr regltabl-header">
+        <div class="regltabl-apprblock">
+            <div class="regltabl-td regltabl-prcful">%Ful</div>
+            <div class="regltabl-td regltabl-prcship">%Ship</div>
+            <div class="regltabl-td regltabl-approval">Approval</div>
+        </div>
+        <div class="regltabl-td regltabl-userprinter">&nbsp;</div>
+        <div class="regltabl-mainblock">
+            <div class="regltabl-td regltabl-brand">&nbsp;</div>
+            <div class="regltabl-td regltabl-rush">&nbsp;</div>
+            <div class="regltabl-td regltabl-order">Order#</div>
+            <div class="regltabl-td regltabl-items">#Items</div>
+            <div class="regltabl-td regltabl-imp">Imp</div>
+            <div class="regltabl-td regltabl-prints">#Prints</div>
+            <div class="regltabl-td regltabl-itmcolor">Item Color/s</div>
+            <div class="regltabl-td regltabl-description shortresched">Item / Description</div>
+            <div class="regltabl-td regltabl-inkcolor">Ink Color/s</div>
+        </div>
+    </div>
+    <div id="printshortunassignarea"></div>
+</div>
