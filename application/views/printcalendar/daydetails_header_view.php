9b0f67cbd8e48e2dadc260edd63cc9d8d1263375
German Polovnikov <polovnikov.g@gmail.com>
2025-10-14 17:57:15 +0300

    Print Schedule New (cosmetic fix)

--- application/views/printcalendar/daydetails_header_view.php
+++ application/views/printcalendar/daydetails_header_view.php
@@ -1,4 +1,4 @@
-<div class="maingrey-title" id="printdate_<?=$printdate?>" ondrop="dropHandler(event)" ondragover="dragoverHandler(event)"><?=$late==0 ? 'TODAY - ' : ''?><?=$title?></div>
+<div class="maingrey-title"><?=$late==0 ? 'TODAY - ' : ''?><?=$title?></div>
 <div class="maingrey-infobox">
     <div class="maingreyinfo-prints"><span><?=$prints==0 ? '-' : QTYOutput($prints)?></span> prints</div>
     <div class="maingreyinfo-items"><span><?=$items==0 ? '-' : QTYOutput($items)?></span> items</div>
--- js/printcalendar/printreshedule.js
+++ js/printcalendar/printreshedule.js
@@ -48,65 +48,75 @@ function dropHandler(ev) {
     ev.preventDefault();
     const data = ev.dataTransfer.getData("text");
     // ev.target.appendChild(document.getElementById(data));
-    // console.log('Add to Element '+ev.target.id+'!');
+    var parentElement = ev.target.closest('.leftsideviewarea');
     var newdate = '';
     var incomeblock = '';
-    if (ev.target.id.substring(0,9)=='printday_') {
-        newdate = ev.target.id.replace('printday_','');
-        incomeblock = 'right';
-    } else {
-        newdate = ev.target.id.replace('printdate_','')
+    if (parentElement) {
+        console.log('Parent '+parentElement.id+'!');
+        newdate = parentElement.id.replace('printdate_','')
         incomeblock = 'left';
-    }
-    var moveorder = '';
-    var outcomeblock = '';
-    if (orderid.substring(0,10)=='shedulord_') {
-        outcomeblock = 'right';
-        moveorder = orderid.replace('shedulord_','');
     } else {
-        outcomeblock = 'left';
-        moveorder = orderid.replace('printord_','');
+        parentElement = ev.target.closest('.rightsideviewarea');
+        if (parentElement.id.substring(0,9)=='printday_') {
+            newdate = parentElement.id.replace('printday_','');
+            incomeblock = 'right';
+        }
     }
-    // Send changes to Scheduler
-    var params = new Array();
-    params.push({name: 'print_date', value: newdate});
-    params.push({name: 'order_id', value: moveorder});
-    params.push({name: 'incomeblock', value: incomeblock});
-    params.push({name: 'outcomeblock', value: outcomeblock});
-    var url = '/printcalendar/ordernewdate';
-    $.post(url, params, function (response){
-         if (response.errors=='') {
-             if (incomeblock==outcomeblock) {
-                 $("div[data-printdata='"+newdate+"']").append(document.getElementById(data))
-             } else {
-                 if (incomeblock=='right') {
-                     if (parseInt(response.data.late)==1) {
-                         $(".dayschedulearea[data-printdata='lateorders']").empty().html(response.data.income);
-                     } else {
-                         $(".dayschedulearea[data-printdata='"+response.data.incomedate+"']").empty().html(response.data.income);
-                     }
-                     $("#printshortunassignarea").empty().html(response.data.unassign);
-                     $("#printshortassignarea").empty().html(response.data.assign);
-                     $(".pscalendar-daybox[data-printdate='"+response.data.outdate+"']").find('div.dayboxorders-numbers').empty().html(response.data.orders);
-                     $(".pscalendar-daybox[data-printdate='"+response.data.outdate+"']").find('div.dayboxprints-numbers').empty().html(response.data.prints);
-                 } else {
-                     if ($("#printshortunassignarea").length==0) {
-                         $(".regular-section").html(response.data.todaytemplate);
-                     }
-                     $("#printshortunassignarea").empty().html(response.data.income);
-                     if (parseInt(response.data.late)==1) {
-                         $(".dayschedulearea[data-printdata='lateorders']").empty().html(response.data.outcome);
-                     } else {
-                         $(".dayschedulearea[data-printdata='"+response.data.outdate+"']").empty().html(response.data.outcome);
-                     }
-                     // Update Calendar
-                     $(".pscalendar-daybox[data-printdate='"+newdate+"']").find('div.dayboxorders-numbers').empty().html(response.data.orders);
-                     $(".pscalendar-daybox[data-printdate='"+newdate+"']").find('div.dayboxprints-numbers').empty().html(response.data.prints);
-                     orderid='';
-                 }
-             }
-         } else {
-             // Show error
-         }
-    },'json');
+    if (incomeblock) {
+        // console.log('Add to Element '+ev.target.id+'!');
+        var moveorder = '';
+        var outcomeblock = '';
+        if (orderid.substring(0,10)=='shedulord_') {
+            outcomeblock = 'right';
+            moveorder = orderid.replace('shedulord_','');
+        } else {
+            outcomeblock = 'left';
+            moveorder = orderid.replace('printord_','');
+        }
+        // Send changes to Scheduler
+        var params = new Array();
+        params.push({name: 'print_date', value: newdate});
+        params.push({name: 'order_id', value: moveorder});
+        params.push({name: 'incomeblock', value: incomeblock});
+        params.push({name: 'outcomeblock', value: outcomeblock});
+        var url = '/printcalendar/ordernewdate';
+        $.post(url, params, function (response){
+            if (response.errors=='') {
+                if (incomeblock==outcomeblock) {
+                    $("div[data-printdata='"+newdate+"']").append(document.getElementById(data))
+                } else {
+                    if (incomeblock=='right') {
+                        if (parseInt(response.data.late)==1) {
+                            $(".dayschedulearea[data-printdata='lateorders']").empty().html(response.data.income);
+                        } else {
+                            $(".dayschedulearea[data-printdata='"+response.data.incomedate+"']").empty().html(response.data.income);
+                        }
+                        $("#printshortunassignarea").empty().html(response.data.unassign);
+                        $("#printshortassignarea").empty().html(response.data.assign);
+                        $(".pscalendar-daybox[data-printdate='"+response.data.outdate+"']").find('div.dayboxorders-numbers').empty().html(response.data.orders);
+                        $(".pscalendar-daybox[data-printdate='"+response.data.outdate+"']").find('div.dayboxprints-numbers').empty().html(response.data.prints);
+                    } else {
+                        if ($("#printshortunassignarea").length==0) {
+                            $(".regular-section").html(response.data.todaytemplate);
+                        }
+                        $("#printshortunassignarea").empty().html(response.data.income);
+                        if (parseInt(response.data.late)==1) {
+                            $(".dayschedulearea[data-printdata='lateorders']").empty().html(response.data.outcome);
+                        } else {
+                            $(".dayschedulearea[data-printdata='"+response.data.outdate+"']").empty().html(response.data.outcome);
+                        }
+                        // Update Calendar
+                        $(".pscalendar-daybox[data-printdate='"+newdate+"']").find('div.dayboxorders-numbers').empty().html(response.data.orders);
+                        $(".pscalendar-daybox[data-printdate='"+newdate+"']").find('div.dayboxprints-numbers').empty().html(response.data.prints);
+                        orderid='';
+                    }
+                }
+            } else {
+                // Show error
+            }
+        },'json');
+    } else {
+        // Income block empty
+        console.log('Income block empty');
+    }
 }
