50ab556e5657d979e65cf2f73487a5af81201f01
German Polovnikov <polovnikov.g@gmail.com>
2025-12-02 15:00:12 +0200

    Double orders test (contacts section)

--- js/projects/dualorders_view.js
+++ js/projects/dualorders_view.js
@@ -1,17 +1,17 @@
 function init_doubleorders() {
-    // $("#dualordspecialtags").find('span.titlebox-arrow').unbind('click').click(function () {
-    //     var blockhide = 0;
-    //     if ($("#dualordspecialtags").hasClass("hidetitle")) {
-    //         blockhide = 1;
-    //     }
-    //     if (blockhide == 1) {
-    //         $("#dualordspecialtags").removeClass("hidetitle");
-    //         $("#dualordspecialtags").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
-    //     } else {
-    //         $("#dualordspecialtags").addClass("hidetitle");
-    //         $("#dualordspecialtags").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
-    //     }
-    // });
+    $("#dualordspecialtags").find('span.titlebox-arrow').unbind('click').click(function () {
+        var blockhide = 0;
+        if ($("#dualordspecialtags").hasClass("hidetitle")) {
+            blockhide = 1;
+        }
+        if (blockhide == 1) {
+            $("#dualordspecialtags").removeClass("hidetitle");
+            $("#dualordspecialtags").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+        } else {
+            $("#dualordspecialtags").addClass("hidetitle");
+            $("#dualordspecialtags").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+        }
+    });
     $("#dualordcredits").find('span.titlebox-arrow').unbind('click').click(function () {
         var blockhide = 0;
         if ($("#dualordcredits").hasClass("hidetitle")) {
@@ -96,19 +96,47 @@ function init_doubleorders() {
             blockhide = 1;
         }
         if (blockhide == 1) {
-            // $("#dualordcustomlabel").find('div.custom-info-name').show();
             $(".tagslist-title").show();
             $(".custinfo-tags").show();
             $(".custinfo-member").show();
             $("#dualordcustomlabel").find('div.custom-info-hide').empty().html('Hide <span class="ci-hideicon"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>');
             $("#dualordcustomlabel").removeClass("hidetitle");
+            // Other Blocks
+            $("#dualordcredits").removeClass("hidetitle");
+            $("#dualordcredits").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+            $("#dualordpaymethods").removeClass("hidetitle");
+            $("#dualordpaymethods").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+            $("#dualordshipaccounts").removeClass("hidetitle");
+            $("#dualordshipaccounts").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+            $("#dualordcontacts").removeClass("hidetitle");
+            $("#dualordcontacts").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+            $("#dualordorderslist").removeClass("hidetitle");
+            $("#dualordorderslist").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+            $("#dualordquotes").removeClass("hidetitle");
+            $("#dualordquotes").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
+            $("#dualordspecialtags").removeClass("hidetitle");
+            $("#dualordspecialtags").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-up" aria-hidden="true"></i>');
         } else {
-            // $("#dualordcustomlabel").find('div.custom-info-name').hide();
             $(".tagslist-title").hide();
             $(".custinfo-tags").hide();
             $(".custinfo-member").hide();
             $("#dualordcustomlabel").find('div.custom-info-hide').empty().html('Show <span class="ci-hideicon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>');
             $("#dualordcustomlabel").addClass("hidetitle");
+            // Other Blocks
+            $("#dualordcredits").addClass("hidetitle");
+            $("#dualordcredits").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+            $("#dualordpaymethods").addClass("hidetitle");
+            $("#dualordpaymethods").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+            $("#dualordshipaccounts").addClass("hidetitle");
+            $("#dualordshipaccounts").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+            $("#dualordcontacts").addClass("hidetitle");
+            $("#dualordcontacts").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+            $("#dualordorderslist").addClass("hidetitle");
+            $("#dualordorderslist").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+            $("#dualordquotes").addClass("hidetitle");
+            $("#dualordquotes").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
+            $("#dualordspecialtags").addClass("hidetitle");
+            $("#dualordspecialtags").find('span.titlebox-arrow').empty().html('<i class="fa fa-chevron-down" aria-hidden="true"></i>');
         }
         init_doubleorders();
     });
