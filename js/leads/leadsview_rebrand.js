49be6d2c69b5bd3272ad67ef37f2120e6a4d2dd5
German Polovnikov <polovnikov.g@gmail.com>
2026-01-22 23:35:37 +0200

    Lift - new Leads view

--- js/leads/leadsview_rebrand.js
+++ js/leads/leadsview_rebrand.js
@@ -356,7 +356,7 @@ function init_interest_management() {
     $("#proofrequesttable").find('div.datarow').unbind('click').click(function (){
         var task = parseInt($(this).data('task'));
         if (task > 0) {
-            // showquotedetails(task);
+            // artproof_lead(task, 'leadsview');
         }
     })
 }
