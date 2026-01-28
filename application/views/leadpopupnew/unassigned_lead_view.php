91f91614d7db5155f5262e620c95acba04736c68
German Polovnikov <polovnikov.g@gmail.com>
2026-01-28 23:02:11 +0200

    New Lead Popup

---
+++ application/views/leadpopupnew/unassigned_lead_view.php
@@ -0,0 +1,8 @@
+<div class="leadreplicaddarea unassigned">
+    <span>UNASSIGNED</span>
+    <?php if ($added==1) : ?>
+    <div class="leadreplicadadd">
+        <i class="fa fa-plus"></i>
+    </div>
+    <?php endif; ?>
+</div>
