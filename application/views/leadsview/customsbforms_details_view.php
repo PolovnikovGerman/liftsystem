d44e4318d09daed754cbd276de628747db528e2d
German Polovnikov <polovnikov.g@gmail.com>
2026-02-06 00:00:38 +0200

    Add Shape type to Custom Quote

--- application/views/leadsview/customsbforms_details_view.php
+++ application/views/leadsview/customsbforms_details_view.php
@@ -33,6 +33,23 @@
                 </div>
             </div>
             <div class="customform-row">
+                <div class="customform-group customform-shape-type">
+                    <label>Type</label>
+                    <select class="custom-input-1" id="shape_type" data-fld="shape_type" data-form="<?=$data['custom_quote_id']?>">
+                        <option value="">Choose closest category</option>
+                        <option value="mascot" <?=$data['shape_type']=='mascot' ? 'selected="selected"' : ''?>>Mascot / Character</option>
+                        <option value="product_replica" <?=$data['shape_type']=='product_replica' ? 'selected="selected"' : ''?>>Product Replica / Industrial</option>
+                        <option value="vehicle" <?=$data['shape_type']=='vehicle' ? 'selected="selected"' : ''?>>Vehicle</option>
+                        <option value="logo" <?=$data['shape_type']=='logo' ? 'selected="selected"' : ''?>>Logo / Words</option>
+                        <option value="food" <?=$data['shape_type']=='food' ? 'selected="selected"' : ''?>>Food & Drink</option>
+                        <option value="buildings" <?=$data['shape_type']=='buildings' ? 'selected="selected"' : ''?>>Buildings / Structures</option>
+                        <option value="people" <?=$data['shape_type']=='people' ? 'selected="selected"' : ''?>>People</option>
+                        <option value="keychain" <?=$data['shape_type']=='keychain' ? 'selected="selected"' : ''?>>Keychain</option>
+                        <option value="other" <?=$data['shape_type']=='other' ? 'selected="selected"' : ''?>>Other</option>
+                    </select>
+                </div>
+            </div>
+            <div class="customform-row">
                 <div class="customform-group">
                     <textarea readonly="readonly"><?=$data['shape_desription']?></textarea>
                 </div>
