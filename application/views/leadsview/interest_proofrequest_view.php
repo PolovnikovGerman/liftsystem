6e3c045452278dc2cd50ee838139915f8a865a94
German Polovnikov <polovnikov.g@gmail.com>
2026-03-27 00:49:49 +0200

    Lead popups fix

--- application/views/leadsview/interest_proofrequest_view.php
+++ application/views/leadsview/interest_proofrequest_view.php
@@ -1,7 +1,13 @@
-<?php $numpp=1;?>
+<?php $numpp=0;?>
+<?php $curdate = '';?>
 <?php foreach ($proofs as $proof) : ?>
-    <div class="datarow <?=$numpp%2==0 ? 'greydatarow' : 'whitedatarow'?>" data-task="<?=$proof['email_id']?>">
-        <div class="newunassign_date_dat"><?=date('m/d/y', strtotime($proof['email_date']));?></div>
+    <div class="datarow <?=$numpp%2==0 ? 'whitedatarow' : 'greydatarow'?> <?=$curdate!==date('m/d/y', strtotime($proof['email_date'])) && $numpp > 0 ? 'topseparaterow' : ''?>" data-task="<?=$proof['email_id']?>">
+        <?php if ($curdate!==date('m/d/y', strtotime($proof['email_date']))) :?>
+            <?php $curdate = date('m/d/y', strtotime($proof['email_date'])); ?>
+            <div class="newunassign_date_dat"><?=$curdate?></div>
+        <?php else : ?>
+            <div class="newunassign_date_dat">--</div>
+        <?php endif; ?>
         <div class="proofrequests_customer_dat truncateoverflowtext" data-event="hover" data-css="itemdetailsballonbox"
              data-bgcolor="#FFFFFF" data-bordercolor="#000" data-position="auto" data-textcolor="#000" data-timer="4000" data-delay="1000"
              data-balloon="<?=empty($proof['email_sendercompany']) ? $proof['email_sender'] : $proof['email_sendercompany']?>">
