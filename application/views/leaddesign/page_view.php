6f0508e6f1ec80ac8ead86d89d6ff20ce37f8dc5
German Polovnikov <polovnikov.g@gmail.com>
2026-04-20 22:58:37 +0300

    Lead design page

--- application/views/leaddesign/page_view.php
+++ application/views/leaddesign/page_view.php
@@ -1,1 +1,11 @@
-<?php
+<?=$menu_view?>
+<div class="contentdata_view">
+    <div class="datarow">
+        <div class="customerdataarea">&nbsp;</div>
+        <div class="emaildataarea">&nbsp;</div>
+    </div>
+    <div class="datarow">
+        <div class="leaddataarea">&nbsp;</div>
+        <div class="orderdataarea">&nbsp;</div>
+    </div>
+</div>
