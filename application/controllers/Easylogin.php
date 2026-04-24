8a061a5b45cf7cd52fafa074c6aba9fed43686c8
German Polovnikov <polovnikov.g@gmail.com>
2026-04-24 21:50:22 +0300

    Easy access to Project page

--- application/controllers/Easylogin.php
+++ application/controllers/Easylogin.php
@@ -1,6 +1,18 @@
 <?php
 
-class Easylogin
+class Easylogin extends Base_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('user_model');
+    }
 
+    public function index()
+    {
+        $token = $this->input->get('token', TRUE);
+        $url = $this->input->get('url', TRUE);
+        log_message('error', 'Easylogin start Token '.$token.' url '.$url);
+        redirect('/');
+    }
 }
