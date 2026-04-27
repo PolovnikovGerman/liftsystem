8a9ea855465ea72b77deb1c36808ffd72da063d2
German Polovnikov <polovnikov.g@gmail.com>
2026-04-27 22:32:36 +0300

    Projects page - new view

--- application/views/dualorders/page_dualorders_view.php
+++ application/views/dualorders/page_dualorders_view.php
@@ -1,1 +1,13 @@
-<?php
+<div class="contant-popup">
+    <div class="section-customer <?=$blocked==0 ? $brandclass : 'blockedcustomer';?>">
+        <?=$customer_view?>
+    </div>
+    <div class="section-content dualblocks <?=$blocked==0 ? $brandclass : 'blockedcustomer';?>">
+        <div class="orderblock">
+            <?=$order_view?>
+        </div>
+        <div class="orderblock">
+            <?=$order_view?>
+        </div>
+    </div>
+</div>
