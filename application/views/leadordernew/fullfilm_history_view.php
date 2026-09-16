899d0b55dd6c1e33fdf621c3887f69b7e11fd1e0
German Polovnikov <polovnikov.g@gmail.com>
2026-09-16 22:16:33 +0300

    New Lead order - add Fulfillment Historical section

--- application/views/leadordernew/fullfilm_history_view.php
+++ application/views/leadordernew/fullfilm_history_view.php
@@ -1,108 +1,108 @@
-<!--<div class="fulfillmentblock">-->
-<div class="fulfillm_historical">
-    <div class="fulfillm_hist_title">HISTORICAL</div>
-    <div class="btbox_list">
-        <div class="btbox btbox_open">
-            <div class="btbox_arrow"><i class="fa fa-caret-right" aria-hidden="true"></i></div>
-            <div class="btbox_title">BT 68485-A</div>
-            <div class="btbox_info">- 09/11/16 - $450.00 - Ariel</div>
-            <div class="btbox_icon"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></div>
-            <div class="btbox_net">.net</div>
-            <div class="btbox_body">
-                <div class="btbox_bodyrow">
-                    <div class="btbox_status">Status:</div>
-                    <div class="btbox_billmethod">Bill Method:</div>
-                    <div class="btbox_rep">Rep:</div>
-                </div>
-                <div class="btbox_bodyrow">
-                    <div class="btbox_shipdate">
-                        <div class="btbox_shipdate_label">Ship Date:</div>
-                        <div class="btbox_shipdate_box">10/24/25</div>
+<div class="fulfillmentblock">
+    <div class="fulfillm_historical">
+        <div class="fulfillm_hist_title">HISTORICAL</div>
+        <div class="btbox_list">
+            <div class="btbox btbox_open">
+                <div class="btbox_arrow"><i class="fa fa-caret-right" aria-hidden="true"></i></div>
+                <div class="btbox_title">BT 68485-A</div>
+                <div class="btbox_info">- 09/11/16 - $450.00 - Ariel</div>
+                <div class="btbox_icon"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></div>
+                <div class="btbox_net">.net</div>
+                <div class="btbox_body">
+                    <div class="btbox_bodyrow">
+                        <div class="btbox_status">Status:</div>
+                        <div class="btbox_billmethod">Bill Method:</div>
+                        <div class="btbox_rep">Rep:</div>
                     </div>
-                    <div class="btbox_shipact">
-                        <div class="btbox_shipact_label">Ship Act:</div>
-                        <select>
-                            <option>DHL 942934274</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="btbox_bodyrow">
-                    <div class="btbox_messagevendor">
-                        <div class="btbox_messagevendor_label">Message to Vendor:</div>
-                        <div class="btbox_messagevendor_box">
-                            <textarea></textarea>
+                    <div class="btbox_bodyrow">
+                        <div class="btbox_shipdate">
+                            <div class="btbox_shipdate_label">Ship Date:</div>
+                            <div class="btbox_shipdate_box">10/24/25</div>
+                        </div>
+                        <div class="btbox_shipact">
+                            <div class="btbox_shipact_label">Ship Act:</div>
+                            <select>
+                                <option>DHL 942934274</option>
+                            </select>
                         </div>
                     </div>
-                </div>
-                <div class="btbox_bodyrow">
-                    <div class="shipaddrs">
-                        <div class="shipaddrs_tabs">
-                            <div class="shipaddrs_tab active">Ship To 1</div>
-                            <div class="shipaddrs_tab">Ship To 2</div>
-                            <div class="shipaddrs_tab">Ship To 3</div>
-                        </div>
-                        <div class="shipaddrs_body">
-                            <div class="shipaddrs_left">
-                                <div class="shipaddrs_addressbox">Bluetrack<br>Attn: Junior<br>855 Bloomfield Ave<br>BT67011A<br>Clifton NJ 07012 - USA</div>
+                    <div class="btbox_bodyrow">
+                        <div class="btbox_messagevendor">
+                            <div class="btbox_messagevendor_label">Message to Vendor:</div>
+                            <div class="btbox_messagevendor_box">
+                                <textarea></textarea>
                             </div>
-                            <div class="shipaddrs_right">
-                                <div class="shipaddrs_method">
-                                    <label>Method:</label>
-                                    <select>
-                                        <option>Ind Expedited</option>
-                                    </select>
+                        </div>
+                    </div>
+                    <div class="btbox_bodyrow">
+                        <div class="shipaddrs">
+                            <div class="shipaddrs_tabs">
+                                <div class="shipaddrs_tab active">Ship To 1</div>
+                                <div class="shipaddrs_tab">Ship To 2</div>
+                                <div class="shipaddrs_tab">Ship To 3</div>
+                            </div>
+                            <div class="shipaddrs_body">
+                                <div class="shipaddrs_left">
+                                    <div class="shipaddrs_addressbox">Bluetrack<br>Attn: Junior<br>855 Bloomfield Ave<br>BT67011A<br>Clifton NJ 07012 - USA</div>
                                 </div>
-                                <div class="shipaddrs_billdate">
-                                    <label>Bill Date:</label>
-                                    <input type="text" name="">
+                                <div class="shipaddrs_right">
+                                    <div class="shipaddrs_method">
+                                        <label>Method:</label>
+                                        <select>
+                                            <option>Ind Expedited</option>
+                                        </select>
+                                    </div>
+                                    <div class="shipaddrs_billdate">
+                                        <label>Bill Date:</label>
+                                        <input type="text" name="">
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="btbox_bodyrow">
-                    <div class="btbox_table">
-                        <div class="btboxtable_header">
-                            <div class="btboxtable_td btboxtable_item">Item#</div>
-                            <div class="btboxtable_td btboxtable_descr">Description</div>
-                            <div class="btboxtable_td btboxtable_qty">Qty</div>
-                            <div class="btboxtable_td btboxtable_price">Price</div>
-                            <div class="btboxtable_td btboxtable_subtotal">Subtotal</div>
-                        </div>
-                        <div class="btboxtable_body">
-                            <div class="btboxtable_tr boldtxt">
-                                <div class="btboxtable_td btboxtable_item">22-ZZ999</div>
-                                <div class="btboxtable_td btboxtable_descr">Custom Wing SBs</div>
-                                <div class="btboxtable_td btboxtable_qty">500</div>
-                                <div class="btboxtable_td btboxtable_price">0.55</div>
-                                <div class="btboxtable_td btboxtable_subtotal">$250.00</div>
+                    <div class="btbox_bodyrow">
+                        <div class="btbox_table">
+                            <div class="btboxtable_header">
+                                <div class="btboxtable_td btboxtable_item">Item#</div>
+                                <div class="btboxtable_td btboxtable_descr">Description</div>
+                                <div class="btboxtable_td btboxtable_qty">Qty</div>
+                                <div class="btboxtable_td btboxtable_price">Price</div>
+                                <div class="btboxtable_td btboxtable_subtotal">Subtotal</div>
                             </div>
-                            <div class="btboxtable_tr grey">
-                                <div class="btboxtable_td btboxtable_item">bt-art</div>
-                                <div class="btboxtable_td btboxtable_descr">Logo Free</div>
-                                <div class="btboxtable_td btboxtable_qty">1</div>
-                                <div class="btboxtable_td btboxtable_price">100</div>
-                                <div class="btboxtable_td btboxtable_subtotal">$100.00</div>
-                            </div>
-                            <div class="btboxtable_tr">
-                                <div class="btboxtable_td btboxtable_item">bt-mold</div>
-                                <div class="btboxtable_td btboxtable_descr">Mold Charges</div>
-                                <div class="btboxtable_td btboxtable_qty">2</div>
-                                <div class="btboxtable_td btboxtable_price">150</div>
-                                <div class="btboxtable_td btboxtable_subtotal">$100.00</div>
+                            <div class="btboxtable_body">
+                                <div class="btboxtable_tr boldtxt">
+                                    <div class="btboxtable_td btboxtable_item">22-ZZ999</div>
+                                    <div class="btboxtable_td btboxtable_descr">Custom Wing SBs</div>
+                                    <div class="btboxtable_td btboxtable_qty">500</div>
+                                    <div class="btboxtable_td btboxtable_price">0.55</div>
+                                    <div class="btboxtable_td btboxtable_subtotal">$250.00</div>
+                                </div>
+                                <div class="btboxtable_tr grey">
+                                    <div class="btboxtable_td btboxtable_item">bt-art</div>
+                                    <div class="btboxtable_td btboxtable_descr">Logo Free</div>
+                                    <div class="btboxtable_td btboxtable_qty">1</div>
+                                    <div class="btboxtable_td btboxtable_price">100</div>
+                                    <div class="btboxtable_td btboxtable_subtotal">$100.00</div>
+                                </div>
+                                <div class="btboxtable_tr">
+                                    <div class="btboxtable_td btboxtable_item">bt-mold</div>
+                                    <div class="btboxtable_td btboxtable_descr">Mold Charges</div>
+                                    <div class="btboxtable_td btboxtable_qty">2</div>
+                                    <div class="btboxtable_td btboxtable_price">150</div>
+                                    <div class="btboxtable_td btboxtable_subtotal">$100.00</div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="btbox">
-            <div class="btbox_arrow"><i class="fa fa-caret-right" aria-hidden="true"></i></div>
-            <div class="btbox_title">BT 68485-B</div>
-            <div class="btbox_info">- 09/25/16 - $250.50 - Mailine</div>
-            <div class="btbox_icon"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></div>
-            <div class="btbox_net">.net</div>
+            <div class="btbox">
+                <div class="btbox_arrow"><i class="fa fa-caret-right" aria-hidden="true"></i></div>
+                <div class="btbox_title">BT 68485-B</div>
+                <div class="btbox_info">- 09/25/16 - $250.50 - Mailine</div>
+                <div class="btbox_icon"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></div>
+                <div class="btbox_net">.net</div>
+            </div>
         </div>
     </div>
 </div>
-<!--</div>-->
