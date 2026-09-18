5d3f7b4dbd711ef77289f7b652b8cffb94a57057
German Polovnikov <polovnikov.g@gmail.com>
2026-09-19 02:12:20 +0300

    New Lead order - show historical fulfillment

--- application/models/Artlead_model.php
+++ application/models/Artlead_model.php
@@ -1984,15 +1984,15 @@ Class Artlead_model extends MY_Model
             $shrtpath=$this->config->item('upload_netpoart_relative');
             if (createPath($shrtpath)) {
                 // po_attach_path
-                $doc_link = str_replace(['../docs/','../../system/docs/'],'http://bluetrack.net/system/docs/', $order['po_attach_name']);
+                $doc_link = str_replace(['../docs/','../../system/docs/'],'http://bluetrack.net/system/docs/', $order['po_attach_path']);
                 $newfile = $fullpath.str_replace([' ','%','"'],'_', $order['po_attach_path']); // po_attach_name
                 echo 'PO ART '.$newfile.'!'.PHP_EOL;
                 echo 'PO Source '.$doc_link.'!'.PHP_EOL;
                 if ($this->_save_remotefile($doc_link, $newfile)) {
                     // Select max numpp
                     $this->db->where('netdata_order_id', $netorder_id);
-                    $this->db->set('po_attach_name', $order['po_attach_path']); // po_attach_name
-                    $this->db->set('po_attach_path', $shrtpath.$order['po_attach_path']); // po_attach_name
+                    $this->db->set('po_attach_name', $order['po_attach_name']); // po_attach_name
+                    $this->db->set('po_attach_path', $shrtpath.$order['po_attach_name']); // po_attach_name
                     $this->db->update('ts_netdata_orders');
                 }
             }
